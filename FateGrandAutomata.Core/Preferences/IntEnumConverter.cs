b7b904ec2245817f0b4dfb1494e647f668388612
Mathew Sachin <mathew.sachin.git@outlook.com>
2020-04-01 18:41:45 +0530

    t

---
+++ FateGrandAutomata.Core/Preferences/IntEnumConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace FateGrandAutomata
+{
+    public class IntEnumConverter : IValueConverter
+    {
+        public object Convert(object Value, Type TargetType, object Parameter, CultureInfo Culture)
+        {
+            return Value is Enum ? (int) Value : 0;
+        }
+
+        public object ConvertBack(object Value, Type TargetType, object Parameter, CultureInfo Culture)
+        {
+            return Value is int ? Enum.ToObject(TargetType, Value) : 0;
+        }
+    }
+}
---
+++ FateGrandAutomata.Core/Preferences/PreferencesViewHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FateGrandAutomata
+{
+    public class PreferencesViewHelper
+    {
+        PreferencesViewHelper() { }
+
+        public static PreferencesViewHelper Instance { get; } = new PreferencesViewHelper();
+
+        public IEnumerable<string> RefillResourceNames { get; } = Enum
+            .GetNames(typeof(RefillResource));
+    }
+}
