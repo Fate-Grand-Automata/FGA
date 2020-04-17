e4aa19834999a2bb963f3f82eb1f1f3fd1706b38
Mathew Sachin <mathew.sachin.git@outlook.com>
2020-04-17 12:54:16 +0530

    works anyway

--- FateGrandAutomata/AutoSkill/CardPriorityActivity.cs
+++ FateGrandAutomata/AutoSkill/CardPriorityActivity.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Android.App;
 using Android.OS;
 using AndroidX.AppCompat.App;
+using AndroidX.Preference;
 using DraggableListView;
 
 namespace FateGrandAutomata
@@ -13,23 +16,63 @@ namespace FateGrandAutomata
         {
             base.OnCreate(SavedInstanceState);
             SetContentView(Resource.Layout.card_priority);
+        }
+
+        static string FilterCapitals<T>(T Obj)
+        {
+            var s = Obj.ToString();
+
+            var sb = new StringBuilder();
+
+            foreach (var t in s)
+            {
+                if (char.IsUpper(t))
+                    sb.Append(t);
+            }
+
+            return sb.ToString();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+
+            if (_cardScores == null)
+                return;
+
+            var val = _cardScores
+                .Select(FilterCapitals)
+                .Aggregate((Result, Current) => $"{Result}, {Current}");
+
+            var preferences = PreferenceManager.GetDefaultSharedPreferences(this);
+            var key = GetString(Resource.String.pref_card_priority);
+            preferences.Edit()
+                .PutString(key, val)
+                .Commit();
+        }
+
+        List<CardScore> _cardScores;
+
+        const string DefaultCardPriority = "WB, WA, WQ, B, A, Q, RB, RA, RQ";
+
+        protected override void OnResume()
+        {
+            base.OnResume();
 
             var list = FindViewById<DraggableListView.DraggableListView>(Resource.Id.card_priority_lv);
 
-            var items = new List<CardScore>
-            {
-                CardScore.WeakBuster,
-                CardScore.WeakArts,
-                CardScore.WeakQuick,
-                CardScore.Buster,
-                CardScore.Arts,
-                CardScore.Quick,
-                CardScore.ResistBuster,
-                CardScore.ResistArts,
-                CardScore.ResistQuick
-            };
+            var preferences = PreferenceManager.GetDefaultSharedPreferences(this);
+            var key = GetString(Resource.String.pref_card_priority);
+            var cardPriority = preferences.GetString(key, DefaultCardPriority);
 
-            list.Adapter = new DraggableListAdapter(this, items);
+            if (cardPriority.Length == 3)
+            {
+                cardPriority = DefaultCardPriority;
+            }
+
+            _cardScores = Card.GetCardScores(cardPriority).ToList();
+
+            list.Adapter = new DraggableListAdapter(this, _cardScores);
         }
     }
 }
