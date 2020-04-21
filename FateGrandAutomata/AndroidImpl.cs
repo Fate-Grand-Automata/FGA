1abede5fff33ad76e7a9d0740387a10b178e8202
Mathew Sachin <MathewSachin@users.noreply.github.com>
2020-04-21 08:44:45 +0530

    Fix: Autosupport scrolling too far (#32)

--- FateGrandAutomata/AndroidImpl.cs
+++ FateGrandAutomata/AndroidImpl.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 using System.IO;
-using System.Threading;
-using Android.AccessibilityServices;
 using Android.OS;
 using Android.Widget;
 using CoreAutomata;
 using Org.Opencv.Android;
 using Environment = Android.OS.Environment;
-using Path = Android.Graphics.Path;
 
 namespace FateGrandAutomata
 {
-    public class AndroidImpl : IPlatformImpl
+    public partial class AndroidImpl : IPlatformImpl
     {
         readonly ScriptRunnerService _accessibilityService;
 
@@ -26,98 +23,12 @@ namespace FateGrandAutomata
 
         public Region WindowRegion => CutoutManager.GetCutoutAppliedRegion(_accessibilityService);
 
-        public void Scroll(Location Start, Location End)
-        {
-            const int duration = 300;
-
-            var swipePath = new Path();
-            swipePath.MoveTo(Start.X, Start.Y);
-            swipePath.LineTo(End.X, End.Y);
-            
-            var gestureBuilder = new GestureDescription.Builder();
-            gestureBuilder.AddStroke(new GestureDescription.StrokeDescription(swipePath, 0, duration));
-            
-            PerformGesture(gestureBuilder.Build());
-        }
-
         readonly Lazy<Handler> _handler = new Lazy<Handler>(() => new Handler(Looper.MainLooper));
 
         public void Toast(string Msg)
         {
             _handler.Value.Post(() =>
                 Android.Widget.Toast.MakeText(_accessibilityService, Msg, ToastLength.Short).Show());
-        }
-
-        public void Click(Location Location)
-        {
-            const int duration = 1;
-
-            var swipePath = new Path();
-            swipePath.MoveTo(Location.X, Location.Y);
-
-            var gestureBuilder = new GestureDescription.Builder();
-            gestureBuilder.AddStroke(new GestureDescription.StrokeDescription(swipePath, 0, duration));
-
-            PerformGesture(gestureBuilder.Build());
-        }
-
-        class GestureCompletedCallback : AccessibilityService.GestureResultCallback
-        {
-            readonly ManualResetEventSlim _event;
-
-            public GestureCompletedCallback(ManualResetEventSlim Event)
-            {
-                _event = Event;
-            }
-
-            public override void OnCompleted(GestureDescription GestureDescription)
-            {
-                _event.Set();
-                base.OnCompleted(GestureDescription);
-            }
-
-            public override void OnCancelled(GestureDescription GestureDescription)
-            {
-                _event.Set();
-                base.OnCancelled(GestureDescription);
-            }
-        }
-
-        readonly ManualResetEventSlim _gestureWaitHandle = new ManualResetEventSlim();
-
-        void PerformGesture(GestureDescription Gesture, bool DoWait = true)
-        {
-            _gestureWaitHandle.Reset();
-
-            _accessibilityService.DispatchGesture(Gesture, new GestureCompletedCallback(_gestureWaitHandle), null);
-
-            if (DoWait)
-            {
-                AutomataApi.Wait(GestureWait);
-            }
-
-            _gestureWaitHandle.Wait();
-        }
-
-        const double GestureWait = 0.3;
-
-        public void ContinueClick(Location Location, int Times)
-        {
-            const int clickTime = 50;
-            const int clickDelay = 10;
-
-            while (Times-- > 0)
-            {
-                var swipePath = new Path();
-                swipePath.MoveTo(Location.X, Location.Y);
-
-                var stroke = new GestureDescription.StrokeDescription(swipePath, clickDelay, clickTime);
-
-                var gestureBuilder = new GestureDescription.Builder()
-                    .AddStroke(stroke);
-
-                PerformGesture(gestureBuilder.Build(), false);
-            }
         }
 
         public IPattern Screenshot()
---
+++ FateGrandAutomata/AndroidImplGestures.cs
@@ -0,0 +1,100 @@
+﻿using System.Threading;
+using Android.AccessibilityServices;
+using Android.Graphics;
+using Android.OS;
+using CoreAutomata;
+
+namespace FateGrandAutomata
+{
+    public partial class AndroidImpl
+    {
+        public void Scroll(Location Start, Location End)
+        {
+            const int swipeDuration = 300;
+            const int holdDuration = 300;
+
+            var gestureBuilder = new GestureDescription.Builder();
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+            {
+                // Android 8+ detected, we can use ContinueStroke
+                var swipePath = new Path();
+                swipePath.MoveTo(Start.X, Start.Y);
+                swipePath.LineTo(End.X, End.Y);
+
+                var swipeStroke = new GestureDescription.StrokeDescription(swipePath, 0, swipeDuration, true);
+                gestureBuilder.AddStroke(swipeStroke);
+
+                // keep the "finger" pressed on the end position for a while
+                var holdPath = new Path();
+                holdPath.MoveTo(End.X, End.Y);
+                var holdStroke = swipeStroke.ContinueStroke(holdPath, swipeDuration, holdDuration, false);
+                gestureBuilder.AddStroke(holdStroke);
+            }
+            else
+            {
+                // Android 7 does not support ContinueStroke, so the only solution is to swipe for half of the intended length
+                var center = new Location((Start.X + End.X) / 2, (Start.Y + End.Y) / 2);
+
+                var swipePath = new Path();
+                swipePath.MoveTo(Start.X, Start.Y);
+                swipePath.LineTo(center.X, center.Y);
+
+                var swipeStroke = new GestureDescription.StrokeDescription(swipePath, 0, swipeDuration);
+                gestureBuilder.AddStroke(swipeStroke);
+            }
+
+            PerformGesture(gestureBuilder.Build());
+        }
+
+        public void Click(Location Location)
+        {
+            const int duration = 1;
+
+            var swipePath = new Path();
+            swipePath.MoveTo(Location.X, Location.Y);
+
+            var gestureBuilder = new GestureDescription.Builder();
+            gestureBuilder.AddStroke(new GestureDescription.StrokeDescription(swipePath, 0, duration));
+
+            PerformGesture(gestureBuilder.Build());
+        }
+
+        readonly ManualResetEventSlim _gestureWaitHandle = new ManualResetEventSlim();
+
+        void PerformGesture(GestureDescription Gesture, bool DoWait = true)
+        {
+            _gestureWaitHandle.Reset();
+
+            _accessibilityService.DispatchGesture(Gesture, new GestureCompletedCallback(_gestureWaitHandle), null);
+
+            if (DoWait)
+            {
+                AutomataApi.Wait(GestureWait);
+            }
+
+            _gestureWaitHandle.Wait();
+        }
+
+        const double GestureWait = 0.3;
+
+        public void ContinueClick(Location Location, int Times)
+        {
+            const int clickTime = 50;
+            const int clickDelay = 10;
+
+            while (Times-- > 0)
+            {
+                var swipePath = new Path();
+                swipePath.MoveTo(Location.X, Location.Y);
+
+                var stroke = new GestureDescription.StrokeDescription(swipePath, clickDelay, clickTime);
+
+                var gestureBuilder = new GestureDescription.Builder()
+                    .AddStroke(stroke);
+
+                PerformGesture(gestureBuilder.Build(), false);
+            }
+        }
+    }
+}
