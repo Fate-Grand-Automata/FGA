2e3374d03cc04db0e25a4dbe50a22bfd20bb963f
Mathew Sachin <mathew.sachin.git@outlook.com>
2020-04-23 13:11:43 +0530

    init

--- FateGrandAutomata/AndroidImplGestures.cs
+++ FateGrandAutomata/AndroidImplGestures.cs
@@ -15,23 +15,23 @@ namespace FateGrandAutomata
 
             var gestureBuilder = new GestureDescription.Builder();
 
-            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
-            {
-                // Android 8+ detected, we can use ContinueStroke
-                var swipePath = new Path();
-                swipePath.MoveTo(Start.X, Start.Y);
-                swipePath.LineTo(End.X, End.Y);
+            //if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+            //{
+            //    // Android 8+ detected, we can use ContinueStroke
+            //    var swipePath = new Path();
+            //    swipePath.MoveTo(Start.X, Start.Y);
+            //    swipePath.LineTo(End.X, End.Y);
 
-                var swipeStroke = new GestureDescription.StrokeDescription(swipePath, 0, swipeDuration, true);
-                gestureBuilder.AddStroke(swipeStroke);
+            //    var swipeStroke = new GestureDescription.StrokeDescription(swipePath, 0, swipeDuration, true);
+            //    gestureBuilder.AddStroke(swipeStroke);
 
-                // keep the "finger" pressed on the end position for a while
-                var holdPath = new Path();
-                holdPath.MoveTo(End.X, End.Y);
-                var holdStroke = swipeStroke.ContinueStroke(holdPath, swipeDuration, holdDuration, false);
-                gestureBuilder.AddStroke(holdStroke);
-            }
-            else
+            //    // keep the "finger" pressed on the end position for a while
+            //    var holdPath = new Path();
+            //    holdPath.MoveTo(End.X, End.Y);
+            //    var holdStroke = swipeStroke.ContinueStroke(holdPath, swipeDuration, holdDuration, false);
+            //    gestureBuilder.AddStroke(holdStroke);
+            //}
+            //else
             {
                 // Android 7 does not support ContinueStroke, so the only solution is to swipe for half of the intended length
                 var center = new Location((Start.X + End.X) / 2, (Start.Y + End.Y) / 2);
