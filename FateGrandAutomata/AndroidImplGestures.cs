35e9dde2c267c8d2f7bc5fadabac37030e4d0fe6
reconman <reconman@users.noreply.github.com>
2020-04-23 22:20:42 +0200

    Combined Android 7 and Android 8 scrolling to save time

--- FateGrandAutomata/AndroidImplGestures.cs
+++ FateGrandAutomata/AndroidImplGestures.cs
@@ -11,44 +11,14 @@ namespace FateGrandAutomata
         public void Scroll(Location Start, Location End)
         {
             const int swipeDuration = 300;
-            const int holdDuration = 300;
+
+            var swipePath = new Path();
+            swipePath.MoveTo(Start.X, Start.Y);
+            swipePath.LineTo(End.X, End.Y);
+            var swipeStroke = new GestureDescription.StrokeDescription(swipePath, 0, swipeDuration);
+            PerformGesture(swipeStroke);
+
             const double scrollWaitTime = 0.7;
-
-            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
-            {
-                // Android 8+ detected, we can use ContinueStroke
-                var swipePath = new Path();
-                swipePath.MoveTo(Start.X, Start.Y);
-                swipePath.LineTo(End.X, End.Y);
-                var swipeStroke = new GestureDescription.StrokeDescription(swipePath, 0, swipeDuration, true);
-                PerformGesture(swipeStroke);
-
-                AutomataApi.Wait(scrollWaitTime);
-
-                // keep the "finger" pressed on the end position for a while
-                var holdPath = new Path();
-                holdPath.MoveTo(End.X, End.Y);
-                var holdStroke = swipeStroke.ContinueStroke(holdPath, 0, holdDuration, false);
-                PerformGesture(holdStroke);
-            }
-            else
-            {
-                // Android 7 does not support ContinueStroke, so the only solution is to swipe for a fraction of the intended length
-                const double fraction = 1 / 1.5;
-                var x = Start.X + (End.X - Start.X) * fraction;
-                var y = Start.Y + (End.Y - Start.Y) * fraction;
-                var end = new Location(x.Round(), y.Round());
-
-                // Let's swipe a bit to the left so that any servant loses focus
-                end.X = 5;
-
-                var swipePath = new Path();
-                swipePath.MoveTo(Start.X, Start.Y);
-                swipePath.LineTo(end.X, end.Y);
-                var swipeStroke = new GestureDescription.StrokeDescription(swipePath, 0, swipeDuration);
-                PerformGesture(swipeStroke);
-            }
-
             AutomataApi.Wait(scrollWaitTime);
         }
 
