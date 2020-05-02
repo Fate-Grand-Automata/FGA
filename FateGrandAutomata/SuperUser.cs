f7b63845fb6a70e2c4f081084a011c19030f52c1
Mathew Sachin <MathewSachin@users.noreply.github.com>
2020-05-02 14:51:45 +0530

    Handle root denied by user (#99)

--- FateGrandAutomata/SuperUser.cs
+++ FateGrandAutomata/SuperUser.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Text;
 using Java.Lang;
-using Exception = Java.Lang.Exception;
+using Exception = System.Exception;
 
 namespace FateGrandAutomata
 {
@@ -17,10 +17,23 @@ namespace FateGrandAutomata
             {
                 _superUser = Runtime.GetRuntime().Exec("su", null, null);
                 _superUserStreamWriter = new StreamWriter(_superUser.OutputStream, Encoding.ASCII);
+
+                // We can check if we got root by sending 'id' to the su process which returns current user's id.
+                // The response should contain 'uid=0', where 0 is the id of root user.
+                _superUserStreamWriter.WriteLine("id");
+                _superUserStreamWriter.Flush();
+
+                // Don't dispose reader or InputStream will be closed
+                var reader = new StreamReader(_superUser.InputStream, Encoding.ASCII);
+                var response = reader.ReadLine();
+                if (response == null || !response.Contains("uid=0"))
+                {
+                    throw new Exception();
+                }
             }
             catch (Exception e)
             {
-                throw new System.Exception("Failed to get Root permission", e);
+                throw new Exception("Failed to get Root permission", e);
             }
         }
 
