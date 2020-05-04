08afea7eca55b3ca3769e7999b2580df1b8f7d4f
Mathew Sachin <mathew.sachin.git@outlook.com>
2020-05-04 14:09:14 +0530

    refactor

--- FateGrandAutomata/AutoskillMakerActivity.cs
+++ FateGrandAutomata/AutoskillMakerActivity.cs
@@ -35,6 +35,7 @@ namespace FateGrandAutomata
         ConstraintLayout _viewMain, _viewAtk, _viewTarget, _viewOrderChange;
         TextView _stageText, _turnText;
         Button[] _xParty, _xSub;
+        RadioButton _n1Radio, _n2Radio;
 
         protected override void OnCreate(Bundle SavedInstanceState)
         {
@@ -56,18 +57,6 @@ namespace FateGrandAutomata
             var np5Btn = FindViewById<ToggleButton>(Resource.Id.np_5);
             var np6Btn = FindViewById<ToggleButton>(Resource.Id.np_6);
 
-            void OnNpClick(string NpCommand)
-            {
-                if (_npSequence.Contains(NpCommand))
-                {
-                    _npSequence = _npSequence.Replace(NpCommand, "") + NpCommand;
-                }
-                else
-                {
-                    _npSequence += NpCommand;
-                }
-            }
-
             np4Btn.Click += (S, E) => OnNpClick("4");
             np5Btn.Click += (S, E) => OnNpClick("5");
             np6Btn.Click += (S, E) => OnNpClick("6");
@@ -86,6 +75,103 @@ namespace FateGrandAutomata
                 ChangeState(AutoskillMakerState.Atk);
             };
 
+            SetupSkills();
+
+            SetupTargets();
+
+            _n1Radio = FindViewById<RadioButton>(Resource.Id.cards_before_np_1);
+            _n2Radio = FindViewById<RadioButton>(Resource.Id.cards_before_np_2);
+
+            var nextBattleBtn = FindViewById<Button>(Resource.Id.autoskill_next_battle_btn);
+            var nextTurnBtn = FindViewById<Button>(Resource.Id.autoskill_next_turn_btn);
+
+            nextBattleBtn.Click += (S, E) =>
+            {
+                ++_stage;
+                OnGoToNext(",#,");
+            };
+
+            nextTurnBtn.Click += (S, E) => OnGoToNext(",");
+
+            var doneBtn = FindViewById<Button>(Resource.Id.autoskill_done_btn);
+            doneBtn.Click += (S, E) =>
+            {
+                AddNpsToSkillCmd();
+
+                Toast.MakeText(ApplicationContext, "Autoskill command copied to clipboard", ToastLength.Short).Show();
+
+                ClipboardManager.FromContext(ApplicationContext).Text = _skillCmd.ToString();
+
+                Finish();
+            };
+
+            enemyTargetGroup.CheckedChange += OnEnemyTargetGroupOnCheckedChange;
+
+            SetupOrderChange();
+        }
+
+        void OnEnemyTargetGroupOnCheckedChange(object S, RadioGroup.CheckedChangeEventArgs E)
+        {
+            if (!(S is RadioGroup group && @group.FindViewById<RadioButton>(E.CheckedId) is { } radioBtn)) return;
+
+            if (!radioBtn.Checked)
+                return;
+
+            void SetEnemyTarget(int Target)
+            {
+                // Merge consecutive target changes
+                if (_skillCmd.Length >= 2 && _skillCmd[_skillCmd.Length - 2] == 't')
+                {
+                    _skillCmd.Remove(_skillCmd.Length - 1, 1)
+                        .Append(Target);
+                }
+                else
+                {
+                    _skillCmd.Append($"t{Target}");
+                }
+            }
+
+            switch (E.CheckedId)
+            {
+                case Resource.Id.enemy_target_1:
+                    SetEnemyTarget(1);
+                    break;
+
+                case Resource.Id.enemy_target_2:
+                    SetEnemyTarget(2);
+                    break;
+
+                case Resource.Id.enemy_target_3:
+                    SetEnemyTarget(3);
+                    break;
+            }
+        }
+
+        void SetupTargets()
+        {
+            void OnTarget(char? TargetCommand)
+            {
+                if (TargetCommand != null)
+                {
+                    _skillCmd.Append(TargetCommand.Value);
+                }
+
+                ChangeState(AutoskillMakerState.Main);
+            }
+
+            var targetNoneBtn = FindViewById<Button>(Resource.Id.no_target_btn);
+            var target1Btn = FindViewById<Button>(Resource.Id.target_1);
+            var target2Btn = FindViewById<Button>(Resource.Id.target_2);
+            var target3Btn = FindViewById<Button>(Resource.Id.target_3);
+
+            targetNoneBtn.Click += (S, E) => OnTarget(null);
+            target1Btn.Click += (S, E) => OnTarget('1');
+            target2Btn.Click += (S, E) => OnTarget('2');
+            target3Btn.Click += (S, E) => OnTarget('3');
+        }
+
+        void SetupSkills()
+        {
             void OnSkill(char SkillCode)
             {
                 _skillCmd.Append(SkillCode);
@@ -118,126 +204,10 @@ namespace FateGrandAutomata
             btnJ.Click += (S, E) => OnSkill('j');
             btnK.Click += (S, E) => OnSkill('k');
             btnL.Click += (S, E) => OnSkill('l');
+        }
 
-            var targetNoneBtn = FindViewById<Button>(Resource.Id.no_target_btn);
-            var target1Btn = FindViewById<Button>(Resource.Id.target_1);
-            var target2Btn = FindViewById<Button>(Resource.Id.target_2);
-            var target3Btn = FindViewById<Button>(Resource.Id.target_3);
-
-            void OnTarget(char? TargetCommand)
-            {
-                if (TargetCommand != null)
-                {
-                    _skillCmd.Append(TargetCommand.Value);
-                }
-
-                ChangeState(AutoskillMakerState.Main);
-            }
-
-            targetNoneBtn.Click += (S, E) => OnTarget(null);
-            target1Btn.Click += (S, E) => OnTarget('1');
-            target2Btn.Click += (S, E) => OnTarget('2');
-            target3Btn.Click += (S, E) => OnTarget('3');
-
-            var n1Radio = FindViewById<RadioButton>(Resource.Id.cards_before_np_1);
-            var n2Radio = FindViewById<RadioButton>(Resource.Id.cards_before_np_2);
-
-            void AddNpsToSkillCmd()
-            {
-                if (_npSequence.Length > 0)
-                {
-                    if (n1Radio.Checked)
-                    {
-                        _skillCmd.Append("n1");
-                    }
-                    else if (n2Radio.Checked)
-                    {
-                        _skillCmd.Append("n2");
-                    }
-                }
-
-                _skillCmd.Append(_npSequence);
-
-                if (_skillCmd.Length >= 1 && _skillCmd[_skillCmd.Length - 1] == ',')
-                {
-                    _skillCmd.Append('0');
-                }
-
-                _npSequence = "";
-            }
-
-            void OnGoToNext(string Separator)
-            {
-                AddNpsToSkillCmd();
-
-                _skillCmd.Append(Separator);
-
-                ++_turn;
-                UpdateStageAndTurn();
-
-                ChangeState(AutoskillMakerState.Main);
-            }
-
-            var nextBattleBtn = FindViewById<Button>(Resource.Id.autoskill_next_battle_btn);
-            var nextTurnBtn = FindViewById<Button>(Resource.Id.autoskill_next_turn_btn);
-
-            nextBattleBtn.Click += (S, E) =>
-            {
-                ++_stage;
-                OnGoToNext(",#,");
-            };
-
-            nextTurnBtn.Click += (S, E) => OnGoToNext(",");
-
-            var doneBtn = FindViewById<Button>(Resource.Id.autoskill_done_btn);
-            doneBtn.Click += (S, E) =>
-            {
-                AddNpsToSkillCmd();
-
-                Toast.MakeText(ApplicationContext, "Autoskill command copied to clipboard", ToastLength.Short).Show();
-
-                ClipboardManager.FromContext(ApplicationContext).Text = _skillCmd.ToString();
-
-                Finish();
-            };
-
-            void SetEnemyTarget(int Target)
-            {
-                // Merge consecutive target changes
-                if (_skillCmd.Length >= 2 && _skillCmd[_skillCmd.Length - 2] == 't')
-                {
-                    _skillCmd.Remove(_skillCmd.Length - 1, 1)
-                        .Append(Target);
-                }
-                else
-                {
-                    _skillCmd.Append($"t{Target}");
-                }
-            }
-
-            enemyTargetGroup.CheckedChange += (S, E) =>
-            {
-                var radioBtn = enemyTargetGroup.FindViewById<RadioButton>(E.CheckedId);
-
-                if (radioBtn == null || !radioBtn.Checked)
-                    return;
-
-                switch (E.CheckedId)
-                {
-                    case Resource.Id.enemy_target_1:
-                        SetEnemyTarget(1);
-                        break;
-
-                    case Resource.Id.enemy_target_2:
-                        SetEnemyTarget(2);
-                        break;
-
-                    case Resource.Id.enemy_target_3:
-                        SetEnemyTarget(3);
-                        break;
-                }
-            };
-
+        void SetupOrderChange()
+        {
             var orderChangeSkillBtn = FindViewById<Button>(Resource.Id.master_x_btn);
             var orderChangeCancelBtn = FindViewById<Button>(Resource.Id.order_change_cancel);
             var orderChangeOkBtn = FindViewById<Button>(Resource.Id.order_change_ok);
@@ -279,13 +249,63 @@ namespace FateGrandAutomata
                 SetOrderChangePartyMember(1);
                 SetOrderChangeSubMember(1);
             };
+
             orderChangeCancelBtn.Click += (S, E) => ChangeState(AutoskillMakerState.Main);
+
             orderChangeOkBtn.Click += (S, E) =>
             {
                 _skillCmd.Append($"x{_xSelectedParty}{_xSelectedSub}");
 
                 ChangeState(AutoskillMakerState.Main);
             };
+        }
+
+        void OnGoToNext(string Separator)
+        {
+            AddNpsToSkillCmd();
+
+            _skillCmd.Append(Separator);
+
+            ++_turn;
+            UpdateStageAndTurn();
+
+            ChangeState(AutoskillMakerState.Main);
+        }
+
+        void AddNpsToSkillCmd()
+        {
+            if (_npSequence.Length > 0)
+            {
+                if (_n1Radio.Checked)
+                {
+                    _skillCmd.Append("n1");
+                }
+                else if (_n2Radio.Checked)
+                {
+                    _skillCmd.Append("n2");
+                }
+            }
+
+            _skillCmd.Append(_npSequence);
+
+            if (_skillCmd.Length >= 1 && _skillCmd[_skillCmd.Length - 1] == ',')
+            {
+                _skillCmd.Append('0');
+            }
+
+            _npSequence = "";
+        }
+
+        void OnNpClick(string NpCommand)
+        {
+            if (_npSequence.Contains(NpCommand))
+            {
+                _npSequence = _npSequence.Replace(NpCommand, "") + NpCommand;
+            }
+            else
+            {
+                _npSequence += NpCommand;
+            }
         }
 
         View GetStateView(AutoskillMakerState State) => State switch
