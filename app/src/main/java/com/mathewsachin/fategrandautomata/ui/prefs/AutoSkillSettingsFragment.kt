b7b7d998b636eeebbde9a62bb56cc729f3dbe404
Mathew Sachin <mathew.sachin.git@outlook.com>
2020-05-22 17:55:01 +0530

    removed selected Autoskill pref from UI

--- app/src/main/java/com/mathewsachin/fategrandautomata/ui/prefs/AutoSkillSettingsFragment.kt
+++ app/src/main/java/com/mathewsachin/fategrandautomata/ui/prefs/AutoSkillSettingsFragment.kt
@@ -2,13 +2,10 @@ package com.mathewsachin.fategrandautomata.ui.prefs
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.mathewsachin.fategrandautomata.R
-import com.mathewsachin.fategrandautomata.scripts.prefs.getStringPref
 import com.mathewsachin.fategrandautomata.ui.AutoSkillListActivity
-import com.mathewsachin.fategrandautomata.util.getAutoSkillEntries
 
 class AutoSkillSettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
@@ -19,21 +16,6 @@ class AutoSkillSettingsFragment : PreferenceFragmentCompat() {
                 startActivity(Intent(activity, AutoSkillListActivity::class.java))
                 true
             }
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        findPreference<ListPreference>(getString(R.string.pref_autoskill_selected))?.apply {
-            val autoSkillItems = getAutoSkillEntries()
-
-            this.entryValues = autoSkillItems.map { it.Id }.toTypedArray()
-            this.entries = autoSkillItems.map { it.Name }.toTypedArray()
-
-            val actual = getStringPref(R.string.pref_autoskill_selected)
-            this.value = "" // Force update
-            this.value = actual
         }
     }
 }
