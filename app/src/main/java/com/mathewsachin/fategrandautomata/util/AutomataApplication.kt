2faf64635bccbccf05205ef8d9bfeb72bf267642
Mathew Sachin <mathew.sachin.git@outlook.com>
2020-06-07 14:55:25 +0530

    tweak

--- app/src/main/java/com/mathewsachin/fategrandautomata/util/AutomataApplication.kt
+++ app/src/main/java/com/mathewsachin/fategrandautomata/util/AutomataApplication.kt
@@ -1,12 +1,16 @@
 package com.mathewsachin.fategrandautomata.util
 
 import android.app.Application
-// import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate
 import org.opencv.android.OpenCVLoader
 
 class AutomataApplication : Application() {
     companion object{
         lateinit var Instance: Application
+    }
+
+    fun forceDarkMode() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 
     override fun onCreate() {
@@ -15,7 +19,6 @@ class AutomataApplication : Application() {
         Instance = this
         OpenCVLoader.initDebug()
 
-        // Uncomment this to test Night Mode
-        // AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        // forceDarkMode()
     }
 }
