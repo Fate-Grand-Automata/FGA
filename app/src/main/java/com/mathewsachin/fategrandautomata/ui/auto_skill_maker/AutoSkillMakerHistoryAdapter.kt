750bc73b9969fe5b4efdb2d61bdc9a4282b11921
Mathew Sachin <mathew.sachin.git@outlook.com>
2020-06-07 14:22:14 +0530

    Different colors

--- app/src/main/java/com/mathewsachin/fategrandautomata/ui/auto_skill_maker/AutoSkillMakerHistoryAdapter.kt
+++ app/src/main/java/com/mathewsachin/fategrandautomata/ui/auto_skill_maker/AutoSkillMakerHistoryAdapter.kt
@@ -3,6 +3,8 @@ package com.mathewsachin.fategrandautomata.ui.auto_skill_maker
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.mathewsachin.fategrandautomata.R
@@ -22,11 +24,28 @@ class AutoSkillMakerHistoryAdapter(private val Items: List<String>)
         val cmd = Items[position]
 
         holder.itemView.let {
-            if (it is CardView) {
-                var color = it.context.getColor(R.color.colorAccent)
+            @ColorInt
+            fun color(@ColorRes Res: Int): Int {
+                return it.context.getColor(Res)
+            }
 
-                if (cmd.contains(',')) {
-                    color = Color.BLACK
+            if (it is CardView) {
+                val color = when {
+                    // Turn/Battle change
+                    cmd.contains(',') -> color(R.color.colorStageChange)
+
+                    // Master Skill
+                    cmd[0] in listOf('j', 'k', 'l', 'x') -> color(R.color.colorMasterSkill)
+
+                    // Enemy Target
+                    cmd[0] == 't' -> color(R.color.colorEnemyTarget)
+
+                    // Servants
+                    cmd[0] in listOf('4', 'a', 'b', 'c') -> color(R.color.colorServant1)
+                    cmd[0] in listOf('5', 'd', 'e', 'f') -> color(R.color.colorServant2)
+                    cmd[0] in listOf('6', 'g', 'h', 'i') -> color(R.color.colorServant3)
+
+                    else -> color(R.color.colorAccent)
                 }
 
                 it.setCardBackgroundColor(color)
