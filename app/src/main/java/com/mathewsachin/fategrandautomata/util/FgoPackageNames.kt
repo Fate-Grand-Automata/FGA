7589d81186d0fd666ff1f20a4e0cb94b21012a91
Mathew Sachin <mathew.sachin.git@outlook.com>
2020-07-17 13:10:53 +0530

    CN, TW pkg names

--- app/src/main/java/com/mathewsachin/fategrandautomata/util/FgoPackageNames.kt
+++ app/src/main/java/com/mathewsachin/fategrandautomata/util/FgoPackageNames.kt
@@ -6,5 +6,7 @@ data class FgoPackageNames(val server: GameServerEnum, val pkgName: String)
 
 val fgoPackageNames = listOf(
     FgoPackageNames(GameServerEnum.En, "com.aniplex.fategrandorder.en"),
-    FgoPackageNames(GameServerEnum.Jp, "com.aniplex.fategrandorder")
+    FgoPackageNames(GameServerEnum.Jp, "com.aniplex.fategrandorder"),
+    FgoPackageNames(GameServerEnum.Cn, "com.bilibili.fatego.sharejoy"),
+    FgoPackageNames(GameServerEnum.Tw, "com.komoe.fgomycard")
 )
