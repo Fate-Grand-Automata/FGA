97e67723847be21895daf299c956df3fc86fed54
Mathew Sachin <MathewSachin@users.noreply.github.com>
2020-07-26 17:06:33 +0530

    Allow deleting multiple AutoSkill configs at once (#203)

--- app/src/main/java/com/mathewsachin/fategrandautomata/dagger/app/ApplicationComponent.kt
+++ app/src/main/java/com/mathewsachin/fategrandautomata/dagger/app/ApplicationComponent.kt
@@ -2,8 +2,8 @@ package com.mathewsachin.fategrandautomata.dagger.app
 
 import com.mathewsachin.fategrandautomata.dagger.service.ScriptRunnerServiceComponent
 import com.mathewsachin.fategrandautomata.ui.AutoSkillItemActivity
-import com.mathewsachin.fategrandautomata.ui.AutoSkillListActivity
 import com.mathewsachin.fategrandautomata.ui.MainActivity
+import com.mathewsachin.fategrandautomata.ui.auto_skill_list.AutoSkillListActivity
 import com.mathewsachin.fategrandautomata.ui.card_priority.CardPriorityActivity
 import com.mathewsachin.fategrandautomata.ui.prefs.AutoSkillItemSettingsFragment
 import com.mathewsachin.fategrandautomata.ui.prefs.MainSettingsFragment
