15a6bdfef22ded92d95b209e374262dc02c8fa4d
Mathew Sachin <mathew.sachin.git@outlook.com>
2020-07-30 12:09:05 +0530

    t

--- scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/enums/CardTypeEnum.kt
+++ scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/enums/CardTypeEnum.kt
@@ -3,5 +3,11 @@ package com.mathewsachin.fategrandautomata.scripts.enums
 enum class CardTypeEnum {
     Buster,
     Arts,
-    Quick
+    Quick,
+
+    /**
+     * Couldn't detect card type.
+     * Can be because Attack screen didn't open up or because the servant is stunned/charmed.
+     */
+    Unknown
 }
