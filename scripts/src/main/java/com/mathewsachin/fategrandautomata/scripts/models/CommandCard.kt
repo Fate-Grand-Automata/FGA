2471a9e851257d681e10ff2e9ed6040fe3fcf8e4
Mathew Sachin <mathew.sachin.git@outlook.com>
2020-08-06 23:05:16 +0530

    Fix: Avoid brave chains

--- scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/models/CommandCard.kt
+++ scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/models/CommandCard.kt
@@ -4,6 +4,7 @@ import com.mathewsachin.libautomata.Location
 import com.mathewsachin.libautomata.Region
 
 class CommandCard private constructor(
+    val index: Int,
     val clickLocation: Location,
     // see docs/card_affinity_regions.png
     val affinityRegion: Region,
@@ -15,6 +16,7 @@ class CommandCard private constructor(
     companion object {
         val list = listOf(
             CommandCard(
+                index = 1,
                 clickLocation = Location(300, 1000),
                 affinityRegion = Region(295, 650, 250, 200),
                 typeRegion = Region(0, 1060, 512, 200),
@@ -22,6 +24,7 @@ class CommandCard private constructor(
                 servantCropRegion = Region(200, 890, 115, 85)
             ),
             CommandCard(
+                index = 2,
                 clickLocation = Location(750, 1000),
                 affinityRegion = Region(810, 650, 250, 200),
                 typeRegion = Region(512, 1060, 512, 200),
@@ -29,6 +32,7 @@ class CommandCard private constructor(
                 servantCropRegion = Region(714, 890, 115, 85)
             ),
             CommandCard(
+                index = 3,
                 clickLocation = Location(1300, 1000),
                 affinityRegion = Region(1321, 650, 250, 200),
                 typeRegion = Region(1024, 1060, 512, 200),
@@ -36,6 +40,7 @@ class CommandCard private constructor(
                 servantCropRegion = Region(1224, 890, 115, 85)
             ),
             CommandCard(
+                index = 4,
                 clickLocation = Location(1800, 1000),
                 affinityRegion = Region(1834, 650, 250, 200),
                 typeRegion = Region(1536, 1060, 512, 200),
@@ -43,6 +48,7 @@ class CommandCard private constructor(
                 servantCropRegion = Region(1738, 890, 115, 85)
             ),
             CommandCard(
+                index = 5,
                 clickLocation = Location(2350, 1000),
                 affinityRegion = Region(2348, 650, 250, 200),
                 typeRegion = Region(2048, 1060, 512, 200),
@@ -51,4 +57,6 @@ class CommandCard private constructor(
             )
         )
     }
+
+    override fun toString() = "[${index}]"
 }
--- scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/models/NoblePhantasm.kt
+++ scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/models/NoblePhantasm.kt
@@ -31,4 +31,6 @@ class NoblePhantasm private constructor(
             )
         )
     }
+
+    override fun toString() = "[${autoSkillCode}]"
 }
