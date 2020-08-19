464f2ceaf98780daf1c63f0959b0de698c3a0b2e
Mathew Sachin <mathew.sachin.git@outlook.com>
2020-08-19 12:27:13 +0530

    t

---
+++ app/src/main/java/com/mathewsachin/fategrandautomata/dagger/app/ViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.mathewsachin.fategrandautomata.dagger.app
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Singleton
+
+@Singleton
+class ViewModelFactory @Inject constructor(
+    private val viewModels: MutableMap<Class<out ViewModel>, Provider<ViewModel>>
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T = viewModels[modelClass]?.get() as T
+}
--- app/src/main/java/com/mathewsachin/fategrandautomata/dagger/app/ViewModelModule.kt
+++ app/src/main/java/com/mathewsachin/fategrandautomata/dagger/app/ViewModelModule.kt
@@ -7,26 +7,8 @@ import com.mathewsachin.fategrandautomata.ui.prefs.AutoSkillItemViewModel
 import com.mathewsachin.fategrandautomata.ui.prefs.FineTuneSettingsViewModel
 import com.mathewsachin.fategrandautomata.ui.prefs.MainSettingsViewModel
 import dagger.Binds
-import dagger.MapKey
 import dagger.Module
 import dagger.multibindings.IntoMap
-import javax.inject.Inject
-import javax.inject.Provider
-import javax.inject.Singleton
-import kotlin.reflect.KClass
-
-@Singleton
-class ViewModelFactory @Inject constructor(
-    private val viewModels: MutableMap<Class<out ViewModel>, Provider<ViewModel>>
-) : ViewModelProvider.Factory {
-
-    override fun <T : ViewModel> create(modelClass: Class<T>): T = viewModels[modelClass]?.get() as T
-}
-
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
-@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
-@MapKey
-internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
 
 @Module
 abstract class ViewModelModule {
