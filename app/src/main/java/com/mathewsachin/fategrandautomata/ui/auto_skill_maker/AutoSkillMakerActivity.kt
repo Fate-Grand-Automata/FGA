123c06448a708b0149ca326f6f5bbf4710973e33
Mathew Sachin <mathew.sachin.git@outlook.com>
2020-08-25 17:55:07 +0530

    phew

--- app/src/main/java/com/mathewsachin/fategrandautomata/ui/auto_skill_maker/AutoSkillMakerActivity.kt
+++ app/src/main/java/com/mathewsachin/fategrandautomata/ui/auto_skill_maker/AutoSkillMakerActivity.kt
@@ -1,17 +1,11 @@
 package com.mathewsachin.fategrandautomata.ui.auto_skill_maker
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.BlendModeColorFilterCompat
-import androidx.core.graphics.BlendModeCompat
-import androidx.lifecycle.observe
 import androidx.navigation.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.mathewsachin.fategrandautomata.R
 import com.mathewsachin.fategrandautomata.databinding.AutoskillMakerBinding
 import com.mathewsachin.fategrandautomata.scripts.prefs.IPreferences
 import dagger.hilt.android.AndroidEntryPoint
@@ -43,43 +37,6 @@ class AutoSkillMakerActivity : AppCompatActivity() {
         recyclerView.adapter = skillCmdVm.adapter
         recyclerView.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-
-        setupOrderChange(binding)
-    }
-
-    private fun setupOrderChange(binding: AutoskillMakerBinding) {
-        val xParty = arrayOf(
-            binding.autoSkillOrderChange.xParty1,
-            binding.autoSkillOrderChange.xParty2,
-            binding.autoSkillOrderChange.xParty3
-        )
-        val xSub = arrayOf(
-            binding.autoSkillOrderChange.xSub1,
-            binding.autoSkillOrderChange.xSub2,
-            binding.autoSkillOrderChange.xSub3
-        )
-
-        skillCmdVm.xSelectedParty.observe(this) {
-            setOrderChangeMember(xParty, it)
-        }
-
-        skillCmdVm.xSelectedSub.observe(this) {
-            setOrderChangeMember(xSub, it)
-        }
-    }
-
-    private fun setOrderChangeMember(Members: Array<Button>, Member: Int) {
-        for ((i, button) in Members.withIndex()) {
-            val selected = i + 1 == Member
-
-            if (selected) {
-                val color = ContextCompat.getColor(this, R.color.colorAccent)
-                button.background.colorFilter = BlendModeColorFilterCompat
-                    .createBlendModeColorFilterCompat(color, BlendModeCompat.SRC)
-            } else {
-                button.background.clearColorFilter()
-            }
-        }
     }
 
     fun onUndo() {
