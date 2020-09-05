b9ad9e4cfd118cf9052202bd1de60934a8ac2cb8
Mathew Sachin <mathew.sachin.git@outlook.com>
2020-09-06 00:51:48 +0530

    Brave Chains moved to per-wave Card Priority in AutoSkill

--- app/src/main/java/com/mathewsachin/fategrandautomata/ui/card_priority/CardPriorityListAdapter.kt
+++ app/src/main/java/com/mathewsachin/fategrandautomata/ui/card_priority/CardPriorityListAdapter.kt
@@ -3,6 +3,9 @@ package com.mathewsachin.fategrandautomata.ui.card_priority
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.widget.SwitchCompat
 import androidx.lifecycle.LifecycleOwner
@@ -12,6 +15,7 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mathewsachin.fategrandautomata.R
+import com.mathewsachin.fategrandautomata.scripts.enums.BraveChainEnum
 import com.mathewsachin.fategrandautomata.util.ItemTouchHelperCallback
 
 class CardPriorityListAdapter(
@@ -26,6 +30,10 @@ class CardPriorityListAdapter(
         val itemsRecyclerView: RecyclerView = ItemView.findViewById(R.id.card_priority_items)
 
         val rearrangeCardsSwitchView: SwitchCompat = ItemView.findViewById(R.id.rearrange_cards)
+
+        val braveChainsLabel: TextView = ItemView.findViewById(R.id.brave_chains_label)
+
+        val braveChainsSpinner: Spinner = ItemView.findViewById(R.id.brave_chains)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -34,7 +42,20 @@ class CardPriorityListAdapter(
 
         return ViewHolder(view).also { holder ->
             experimental.observe(lifecycleOwner) {
-                holder.rearrangeCardsSwitchView.visibility = if (it) View.VISIBLE else View.GONE
+                val visible = if (it) View.VISIBLE else View.GONE
+                holder.rearrangeCardsSwitchView.visibility = visible
+                holder.braveChainsLabel.visibility = visible
+                holder.braveChainsSpinner.visibility = visible
+            }
+
+            ArrayAdapter.createFromResource(
+                holder.itemView.context,
+                R.array.brave_chain_labels,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+                holder.braveChainsSpinner.adapter = adapter
             }
         }
     }
@@ -49,6 +70,21 @@ class CardPriorityListAdapter(
             Items[position].rearrangeCards = isChecked
         }
         holder.rearrangeCardsSwitchView.isChecked = Items[position].rearrangeCards
+
+        holder.braveChainsSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+                val value = BraveChainEnum.values()[pos]
+
+                Items[position].braveChains = value
+
+                holder.rearrangeCardsSwitchView.isEnabled = value != BraveChainEnum.Avoid
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                holder.braveChainsSpinner.setSelection(0)
+            }
+        }
+        holder.braveChainsSpinner.setSelection(Items[position].braveChains.ordinal)
 
         val adapter = CardPriorityAdapter(Items[position].scores)
 
