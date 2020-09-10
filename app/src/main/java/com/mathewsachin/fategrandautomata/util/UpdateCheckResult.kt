bcdee159c27ab3be918e59bc206778bb75b74fb0
Mathew Sachin <mathew.sachin.git@outlook.com>
2020-09-10 12:22:33 +0530

    removed update check

--- app/src/main/java/com/mathewsachin/fategrandautomata/util/UpdateCheckResult.kt
+++
@@ -1,7 +0,0 @@
-package com.mathewsachin.fategrandautomata.util
-
-sealed class UpdateCheckResult {
-    data class Available(val version: String) : UpdateCheckResult()
-    object NotAvailable : UpdateCheckResult()
-    data class Failed(val e: Exception) : UpdateCheckResult()
-}
