f83ce31e1d0e184b5d36e9df766311dcef145359
Mathew Sachin <mathew.sachin.git@outlook.com>
2020-09-16 01:39:26 +0530

    works atleast

--- scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/entrypoints/AutoGiftBox.kt
+++ scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/entrypoints/AutoGiftBox.kt
@@ -12,9 +12,9 @@ class AutoGiftBox @Inject constructor(
 ) : EntryPoint(exitManager, platformImpl, fgAutomataApi.messages), IFgoAutomataApi by fgAutomataApi {
 
     companion object {
-        const val goldThreshold = 3
+        const val goldThreshold = 1
         const val maxClickCount = 99
-        val checkRegion = Region(820, 225, 60, 1050)
+        val checkRegion = Region(820, 225, 60, 1050) * 2.0
     }
 
     private var clickCount = 0
@@ -31,16 +31,16 @@ class AutoGiftBox @Inject constructor(
 
     private val countRegionX
         get() = when (prefs.gameServer) {
-            GameServerEnum.Jp -> 330
-            GameServerEnum.En -> 400
-            else -> 420
+            GameServerEnum.Jp -> 660
+            GameServerEnum.En -> 800
+            else -> 840
         }
 
     private fun checkGifts() {
         for (gift in checkRegion.findAll(images.giftBoxCheck)) {
-            val countRegion = Region(countRegionX, gift.Region.Y - 50, 100, 30)
-            val iconRegion = Region(95, gift.Region.Y - 58, 200, 120)
-            val clickSpot = Location(850, gift.Region.Y + 25)
+            val countRegion = Region(countRegionX, gift.Region.Y - 120, 400, 80)
+            val iconRegion = Region(190, gift.Region.Y - 116, 400, 240)
+            val clickSpot = Location(1700, gift.Region.Y + 50)
 
             val gold = images.goldXP in iconRegion
             val silver = !gold && images.silverXP in iconRegion
@@ -53,7 +53,7 @@ class AutoGiftBox @Inject constructor(
                         3 to images.x3,
                         4 to images.x4
                     ).entries.firstOrNull { (_, pattern) ->
-                        pattern in iconRegion
+                        pattern in countRegion
                     }?.key
 
                     if (count == null || count > goldThreshold) {
