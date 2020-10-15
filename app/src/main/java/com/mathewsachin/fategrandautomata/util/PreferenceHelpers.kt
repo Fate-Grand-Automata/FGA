b3391507400dd215506d1ffaef6a40559a491643
Mathew Sachin <MathewSachin@users.noreply.github.com>
2020-10-15 12:06:45 +0530

    Separate page under AutoSkill config for Preferred Support Settings (#516)
    For reducing clutter in AutoSkill config settings

--- app/src/main/java/com/mathewsachin/fategrandautomata/util/PreferenceHelpers.kt
+++ app/src/main/java/com/mathewsachin/fategrandautomata/util/PreferenceHelpers.kt
@@ -3,10 +3,7 @@ package com.mathewsachin.fategrandautomata.util
 import android.text.InputType
 import androidx.preference.EditTextPreference
 import androidx.preference.MultiSelectListPreference
-import androidx.preference.PreferenceFragmentCompat
-import com.mathewsachin.fategrandautomata.StorageDirs
 import java.io.File
-import com.mathewsachin.fategrandautomata.prefs.R.string as prefKeys
 
 fun EditTextPreference.makeNumeric() {
     setOnBindEditTextListener {
@@ -20,28 +17,7 @@ fun EditTextPreference.makeMultiLine() {
     }
 }
 
-fun PreferenceFragmentCompat.findServantList() =
-    findPreference<MultiSelectListPreference>(getString(prefKeys.pref_support_pref_servant))
-
-fun PreferenceFragmentCompat.findCeList() =
-    findPreference<MultiSelectListPreference>(getString(prefKeys.pref_support_pref_ce))
-
-fun PreferenceFragmentCompat.findFriendNamesList() =
-    findPreference<MultiSelectListPreference>(getString(prefKeys.pref_support_friend_names))
-
-fun PreferenceFragmentCompat.preferredSupportOnCreate() {
-    val servants = findServantList() ?: return
-    servants.summaryProvider = SupportMultiSelectListSummaryProvider()
-
-    val ces = findCeList() ?: return
-    ces.summaryProvider = SupportMultiSelectListSummaryProvider()
-
-    findFriendNamesList()?.apply {
-        summaryProvider = SupportMultiSelectListSummaryProvider()
-    }
-}
-
-private fun MultiSelectListPreference.populateFriendOrCe(ImgFolder: File) {
+fun MultiSelectListPreference.populateFriendOrCe(ImgFolder: File) {
     val entries = (ImgFolder.listFiles() ?: emptyArray())
         .filter { it.isFile }
         .sortedWith(compareBy(String.CASE_INSENSITIVE_ORDER) { it.name })
@@ -55,28 +31,4 @@ private fun MultiSelectListPreference.populateFriendOrCe(ImgFolder: File) {
     this.entries = entries
         .map { it.nameWithoutExtension }
         .toTypedArray()
-}
-
-fun PreferenceFragmentCompat.preferredSupportOnResume(storageDirs: StorageDirs) {
-    val servants = findServantList() ?: return
-    val ces = findCeList() ?: return
-    val friendNames = findFriendNamesList() ?: return
-
-    servants.apply {
-        val entries = (storageDirs.supportServantImgFolder.listFiles() ?: emptyArray())
-            .map { it.name }
-            .sortedWith(compareBy(String.CASE_INSENSITIVE_ORDER) { it })
-            .toTypedArray()
-
-        this.entryValues = entries
-        this.entries = entries
-    }
-
-    ces.apply {
-        populateFriendOrCe(storageDirs.supportCeFolder)
-    }
-
-    friendNames.apply {
-        populateFriendOrCe(storageDirs.supportFriendFolder)
-    }
 }
