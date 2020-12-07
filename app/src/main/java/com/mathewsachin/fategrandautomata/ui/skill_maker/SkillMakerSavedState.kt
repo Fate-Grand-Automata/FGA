8517fcecf10db3f8d7e6eeae70e7676dfd63caad
Mathew Sachin <mathew.sachin.git@outlook.com>
2020-12-08 00:06:41 +0530

    t

--- app/src/main/java/com/mathewsachin/fategrandautomata/ui/skill_maker/SkillMakerSavedState.kt
+++ app/src/main/java/com/mathewsachin/fategrandautomata/ui/skill_maker/SkillMakerSavedState.kt
@@ -1,7 +1,7 @@
 package com.mathewsachin.fategrandautomata.ui.skill_maker
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class SkillMakerSavedState(
