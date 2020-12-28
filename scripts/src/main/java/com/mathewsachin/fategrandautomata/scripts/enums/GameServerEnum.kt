c2eb0c4edb4cd09eda06762711eb30599b2c2f72
neobenedict <16228871+neobenedict@users.noreply.github.com>
2020-12-28 02:51:23 +0000

    Add betterfgo APK support

--- scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/enums/GameServerEnum.kt
+++ scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/enums/GameServerEnum.kt
@@ -1,8 +1,8 @@
 package com.mathewsachin.fategrandautomata.scripts.enums
 
 enum class GameServerEnum constructor(vararg val packageNames: String) {
-    En("com.aniplex.fategrandorder.en"),
-    Jp("com.aniplex.fategrandorder"),
+    En("com.aniplex.fategrandorder.en", "io.rayshift.betterfgo.en"),
+    Jp("com.aniplex.fategrandorder", "io.rayshift.betterfgo"),
     Cn("com.bilibili.fatego", "com.bilibili.fatego.sharejoy"),
     Tw("com.komoe.fgomycard", "com.xiaomeng.fategrandorder"),
     Kr("com.netmarble.fgok");
