6d54c439ad0d37fc47a97c574a2c71907bfec960
reconman <reconman@users.noreply.github.com>
2020-12-31 13:58:26 +0100

    Added X offset for CN

--- scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/entrypoints/AutoGiftBox.kt
+++ scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/entrypoints/AutoGiftBox.kt
@@ -76,10 +76,9 @@ class AutoGiftBox @Inject constructor(
 
         for (gift in checkRegion.findAll(images.giftBoxCheck).sorted()) {
             val countRegion = when (prefs.gameServer) {
-                GameServerEnum.Jp -> -970
+                GameServerEnum.Jp, GameServerEnum.Tw, GameServerEnum.Cn -> -940
                 GameServerEnum.En -> -830
                 GameServerEnum.Kr -> -960
-                GameServerEnum.Tw -> -930
                 else -> throw ScriptExitException("Not supported on this server yet")
             }.let { x -> Region(x, -120, 300, 100) } + gift.Region.location
 
