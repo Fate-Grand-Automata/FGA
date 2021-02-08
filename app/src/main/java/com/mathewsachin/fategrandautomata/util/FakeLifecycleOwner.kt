e89dc7972871627718d452d30060997aa5eaf972
Mathew Sachin <mathew.sachin.git@outlook.com>
2021-02-08 14:14:25 +0530

    fake lifecycle owner

---
+++ app/src/main/java/com/mathewsachin/fategrandautomata/util/FakeLifecycleOwner.kt
@@ -0,0 +1,52 @@
+package com.mathewsachin.fategrandautomata.util
+
+// From: https://gist.github.com/handstandsam/6ecff2f39da72c0b38c07aa80bbb5a2f
+
+import android.content.Context
+import android.os.Bundle
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.lifecycle.*
+import androidx.savedstate.SavedStateRegistryController
+import androidx.savedstate.SavedStateRegistryOwner
+import androidx.savedstate.ViewTreeSavedStateRegistryOwner
+
+class FakeLifecycleOwner : SavedStateRegistryOwner {
+    private var lifecycleRegistry = LifecycleRegistry(this)
+    private var savedStateRegistryController = SavedStateRegistryController.create(this)
+
+    override fun getLifecycle() = lifecycleRegistry
+
+    fun setCurrentState(state: Lifecycle.State) {
+        lifecycleRegistry.currentState = state
+    }
+
+    fun handleLifecycleEvent(event: Lifecycle.Event) {
+        lifecycleRegistry.handleLifecycleEvent(event)
+    }
+
+    override fun getSavedStateRegistry() =
+        savedStateRegistryController.savedStateRegistry
+
+    fun performRestore(savedState: Bundle?) {
+        savedStateRegistryController.performRestore(savedState)
+    }
+
+    fun performSave(outBundle: Bundle) {
+        savedStateRegistryController.performSave(outBundle)
+    }
+}
+
+fun Context.fakedComposeView(view: @Composable () -> Unit) =
+    ComposeView(this).also {
+        it.setContent { view() }
+
+        // Trick The ComposeView into thinking we are tracking lifecycle
+        val viewModelStore = ViewModelStore()
+        val lifecycleOwner = FakeLifecycleOwner()
+        lifecycleOwner.performRestore(null)
+        lifecycleOwner.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+        ViewTreeLifecycleOwner.set(it, lifecycleOwner)
+        ViewTreeViewModelStoreOwner.set(it) { viewModelStore }
+        ViewTreeSavedStateRegistryOwner.set(it, lifecycleOwner)
+    }
