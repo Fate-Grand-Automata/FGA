026f8458e007381256714d8e874182e68ed9c5f7
Mathew Sachin <mathew.sachin.git@outlook.com>
2021-07-31 15:08:32 +0530

    Fix: Stepper doesn't update backing field if it gets disabled while pressed (fixes #774)

--- app/src/main/java/com/mathewsachin/fategrandautomata/ui/HoldRepeatClickable.kt
+++ app/src/main/java/com/mathewsachin/fategrandautomata/ui/HoldRepeatClickable.kt
@@ -6,10 +6,14 @@ import androidx.compose.foundation.indication
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.material.ripple.rememberRipple
-import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.input.pointer.pointerInput
+import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.time.Duration
@@ -20,20 +24,12 @@ fun Modifier.holdRepeatClickable(
     enabled: Boolean = true
 ) = composed {
     // rememberUpdatedState is needed on State objects accessed from the gesture callback,
-    // otherwise the callback itself would get recomposed.
+    // otherwise the callback would only get the outdated values.
     val rememberedIsEnabled by rememberUpdatedState(enabled)
     val rememberedOnRepeat by rememberUpdatedState(onRepeat)
     val rememberedOnEnd by rememberUpdatedState(onEnd)
 
     val interactionSource = remember { MutableInteractionSource() }
-    var pressedInteraction by remember { mutableStateOf<PressInteraction.Press?>(null) }
-    DisposableEffect(interactionSource) {
-        onDispose {
-            pressedInteraction?.let {
-                interactionSource.tryEmit(PressInteraction.Cancel(it))
-            }
-        }
-    }
 
     val longPressTimeout = Duration.milliseconds(ViewConfiguration.getLongPressTimeout())
     val repeatIntervalDelta = Duration.milliseconds(2)
@@ -45,52 +41,49 @@ fun Modifier.holdRepeatClickable(
         .pointerInput(true) {
             detectTapGestures(onPress = { offset ->
                 if (rememberedIsEnabled) {
-                    val currentJob = scope.launch {
-                        var first = false
+                    try {
+                        val currentJob = scope.launch {
+                            var first = false
 
-                        try {
-                            delay(longPressTimeout)
-                            var repeatInterval = Duration.milliseconds(100)
+                            try {
+                                delay(longPressTimeout)
+                                var repeatInterval = Duration.milliseconds(100)
 
-                            while (true) {
-                                rememberedOnRepeat()
-                                @Suppress("UNUSED_VALUE")
-                                first = true
+                                while (rememberedIsEnabled) {
+                                    rememberedOnRepeat()
+                                    first = true
 
-                                delay(repeatInterval)
+                                    delay(repeatInterval)
 
-                                repeatInterval = (repeatInterval - repeatIntervalDelta)
-                                    .coerceAtLeast(minRepeatInterval)
+                                    repeatInterval = (repeatInterval - repeatIntervalDelta)
+                                        .coerceAtLeast(minRepeatInterval)
+                                }
+                            } finally {
+                                if (!first) {
+                                    rememberedOnRepeat()
+                                }
                             }
-                        } catch (e: Exception) {
-                            // Ignore
                         }
 
-                        if (!first) {
-                            rememberedOnRepeat()
-                        }
+                        val pressInteraction = PressInteraction.Press(offset)
+                        interactionSource.emit(pressInteraction)
+
+                        val success = tryAwaitRelease()
+
+                        val endInteraction =
+                            if (success) {
+                                PressInteraction.Release(pressInteraction)
+                            } else {
+                                PressInteraction.Cancel(pressInteraction)
+                            }
+
+                        interactionSource.emit(endInteraction)
+
+                        currentJob.cancelAndJoin()
                     }
-
-                    val pressInteraction = PressInteraction.Press(offset)
-                    interactionSource.emit(pressInteraction)
-                    pressedInteraction = pressInteraction
-
-                    val success = tryAwaitRelease()
-
-                    val endInteraction =
-                        if (success) {
-                            PressInteraction.Release(pressInteraction)
-                        } else {
-                            PressInteraction.Cancel(pressInteraction)
-                        }
-
-                    interactionSource.emit(endInteraction)
-                    pressedInteraction = null
-
-                    currentJob.cancel()
-                    currentJob.join()
-
-                    rememberedOnEnd()
+                    finally {
+                        rememberedOnEnd()
+                    }
                 }
             })
         }
