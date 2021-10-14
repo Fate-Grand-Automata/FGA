ca1eb7e84753ec66c18d4700bed1e9eaa479de5d
Mathew Sachin <mathew.sachin.git@outlook.com>
2021-10-14 21:02:52 +0530

    Extract party selection logic into a separate class

--- scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/entrypoints/AutoBattle.kt
+++ scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/entrypoints/AutoBattle.kt
@@ -3,7 +3,6 @@ package com.mathewsachin.fategrandautomata.scripts.entrypoints
 import com.mathewsachin.fategrandautomata.IStorageProvider
 import com.mathewsachin.fategrandautomata.scripts.IFgoAutomataApi
 import com.mathewsachin.fategrandautomata.scripts.Images
-import com.mathewsachin.fategrandautomata.scripts.ScriptLog
 import com.mathewsachin.fategrandautomata.scripts.ScriptNotify
 import com.mathewsachin.fategrandautomata.scripts.enums.GameServerEnum
 import com.mathewsachin.fategrandautomata.scripts.enums.MaterialEnum
@@ -16,7 +15,6 @@ import com.mathewsachin.fategrandautomata.scripts.prefs.IPreferences
 import com.mathewsachin.libautomata.*
 import com.mathewsachin.libautomata.dagger.ScriptScope
 import javax.inject.Inject
-import kotlin.math.absoluteValue
 import kotlin.time.Duration
 
 /**
@@ -43,7 +41,8 @@ class AutoBattle @Inject constructor(
     private val battle: Battle,
     private val support: Support,
     private val battleConfig: IBattleConfig,
-    private val withdraw: Withdraw
+    private val withdraw: Withdraw,
+    private val partySelection: PartySelection
 ) : EntryPoint(exitManager), IFgoAutomataApi by fgAutomataApi {
     sealed class ExitReason {
         object Abort : ExitReason()
@@ -69,7 +68,6 @@ class AutoBattle @Inject constructor(
 
     private var stonesUsed = 0
     private var isContinuing = false
-    private var partySelected = false
     private var ceDropCount = 0
 
     // Set all Materials to 0
@@ -476,63 +474,13 @@ class AutoBattle @Inject constructor(
     }
 
     /**
-     * Selects the party for the quest based on the AutoSkill configuration.
-     *
-     * The possible behaviors of this method are:
-     * 1. If no value is specified, the currently selected party is used.
-     * 2. If a value is specified and is the same as the currently selected party, the party is not
-     * changed.
-     * 3. If a value is specified and is different than the currently selected party, the party is
-     * changed to the configured one by clicking on the little dots above the party names.
-     */
-    fun selectParty() {
-        val party = battleConfig.party
-
-        if (!partySelected && party in game.partySelectionArray.indices) {
-            val currentParty = game.selectedPartyRegion
-                .find(images[Images.SelectedParty])
-                ?.let { match ->
-                    // Find party with min distance from center of matched region
-                    game.partySelectionArray.withIndex().minByOrNull {
-                        (it.value.x - match.region.center.x).absoluteValue
-                    }?.index
-                }
-
-            messages.log(
-                ScriptLog.CurrentParty(currentParty)
-            )
-
-            /* If the currently selected party cannot be detected, we need to switch to a party
-               which was not configured. The reason is that the "Start Quest" button becomes
-               unresponsive if you switch from a party to the same one. */
-            if (currentParty == null) {
-                val tempParty = if (party == 0) 1 else 0
-                game.partySelectionArray[tempParty].click()
-
-                Duration.seconds(1).wait()
-            }
-
-            // Switch to the configured party
-            if (currentParty != party) {
-                game.partySelectionArray[party].click()
-
-                Duration.seconds(1.2).wait()
-            }
-
-            /* If we select the party once, the same party will be used by the game for next fight.
-               So, we don't have to select it again. */
-            partySelected = true
-        }
-    }
-
-    /**
      * Starts the quest after the support has already been selected. The following features are done optionally:
      * 1. The configured party is selected if it is set in the selected AutoSkill config
      * 2. A boost item is selected if [IPreferences.boostItemSelectionMode] is set (needed in some events)
      * 3. The story is skipped if [IPreferences.storySkip] is activated
      */
     private fun startQuest() {
-        selectParty()
+        partySelection.selectParty()
 
         game.menuStartQuestClick.click()
 
