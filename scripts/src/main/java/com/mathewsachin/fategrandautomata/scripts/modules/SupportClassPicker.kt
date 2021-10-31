daae932dedbde5e412df69f476edd9b31540eab5
Mathew Sachin <mathew.sachin.git@outlook.com>
2021-10-31 12:12:04 +0530

    Switch to selected support class (including Also check all) instead of refreshing immediately if no matching support found

---
+++ scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/modules/SupportClassPicker.kt
@@ -0,0 +1,27 @@
+package com.mathewsachin.fategrandautomata.scripts.modules
+
+import com.mathewsachin.fategrandautomata.scripts.IFgoAutomataApi
+import com.mathewsachin.fategrandautomata.scripts.enums.SupportClass
+import com.mathewsachin.fategrandautomata.scripts.enums.canAlsoCheckAll
+import com.mathewsachin.fategrandautomata.scripts.prefs.ISupportPreferences
+import com.mathewsachin.libautomata.dagger.ScriptScope
+import javax.inject.Inject
+import kotlin.time.Duration
+
+@ScriptScope
+class SupportClassPicker @Inject constructor(
+    fgAutomataApi: IFgoAutomataApi,
+    private val supportPrefs: ISupportPreferences
+) : IFgoAutomataApi by fgAutomataApi {
+    fun selectSupportClass(supportClass: SupportClass = supportPrefs.supportClass) {
+        if (supportClass == SupportClass.None)
+            return
+
+        locations.support.locate(supportClass).click()
+
+        Duration.seconds(0.5).wait()
+    }
+
+    fun shouldAlsoCheckAll() =
+        supportPrefs.alsoCheckAll && supportPrefs.supportClass.canAlsoCheckAll
+}
