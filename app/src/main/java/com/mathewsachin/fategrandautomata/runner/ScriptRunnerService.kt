a15deb9930456c3e5482789968f79235e8bf73c5
Mathew Sachin <mathew.sachin.git@outlook.com>
2021-11-06 23:20:29 +0530

    ScriptRunnerServiceController

--- app/src/main/java/com/mathewsachin/fategrandautomata/runner/ScriptRunnerService.kt
+++ app/src/main/java/com/mathewsachin/fategrandautomata/runner/ScriptRunnerService.kt
@@ -8,23 +8,8 @@ import android.os.IBinder
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.core.content.ContextCompat
-import com.mathewsachin.fategrandautomata.R
-import com.mathewsachin.fategrandautomata.prefs.core.GameAreaMode
-import com.mathewsachin.fategrandautomata.prefs.core.PrefsCore
-import com.mathewsachin.fategrandautomata.util.DisplayHelper
-import com.mathewsachin.fategrandautomata.util.ImageLoader
-import com.mathewsachin.fategrandautomata.util.ScreenOffReceiver
-import com.mathewsachin.fategrandautomata.util.ScriptMessages
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import timber.log.Timber
-import timber.log.info
-import timber.log.verbose
 import javax.inject.Inject
-import kotlin.time.Duration
 
 @AndroidEntryPoint
 class ScriptRunnerService: Service() {
@@ -33,7 +18,7 @@ class ScriptRunnerService: Service() {
         val serviceStarted: State<Boolean> = mServiceStarted
 
         private var instance: ScriptRunnerService? = null
-            set(value) {
+            private set(value) {
                 field = value
                 mServiceStarted.value = value != null
             }
@@ -55,115 +40,28 @@ class ScriptRunnerService: Service() {
         var mediaProjectionToken: Intent? = null
             set(value) {
                 field = value
-                instance?.screenshotServiceHolder?.prepareScreenshotService()
+                instance?.controller?.onNewMediaProjectionToken()
             }
     }
 
     @Inject
-    lateinit var imageLoader: ImageLoader
-
-    @Inject
-    lateinit var prefsCore: PrefsCore
-
-    @Inject
-    lateinit var overlay: ScriptRunnerOverlay
-
-    @Inject
-    lateinit var scriptManager: ScriptManager
-
-    @Inject
-    lateinit var notification: ScriptRunnerNotification
-
-    @Inject
-    lateinit var messages: ScriptMessages
-
-    @Inject
-    lateinit var messageBox: ScriptRunnerMessageBox
-
-    @Inject
-    lateinit var screenshotServiceHolder: ScreenshotServiceHolder
-
-    @Inject
-    lateinit var displayHelper: DisplayHelper
-
-    private val screenOffReceiver = ScreenOffReceiver()
-
-    private val scope = CoroutineScope(Dispatchers.Default)
+    lateinit var controller: ScriptRunnerServiceController
 
     override fun onDestroy() {
-        Timber.info { "Script runner service destroyed" }
-
-        scriptManager.stopScript()
-        screenshotServiceHolder.close()
-
-        imageLoader.clearImageCache()
-
-        overlay.hide()
-
-        screenOffReceiver.unregister(this)
+        controller.onDestroy()
         instance = null
-
         super.onDestroy()
     }
 
     override fun onBind(intent: Intent?): IBinder? = null
 
     override fun onCreate() {
-        Timber.info { "Script runner service created" }
         super.onCreate()
         instance = this
-        notification.show()
-
-        screenOffReceiver.register(this) {
-            Timber.verbose { "SCREEN OFF" }
-
-            scriptManager.pause(ScriptManager.PauseAction.Pause).let { success ->
-                if (success) {
-                    val title = getString(R.string.script_paused)
-                    val msg = getString(R.string.screen_turned_off)
-                    messages.notify(msg)
-
-                    scope.launch {
-                        messageBox.show(title, msg)
-                    }
-                }
-            }
-        }
-
-        if (shouldDisplayPlayButton()) {
-            overlay.show()
-        }
-
-        screenshotServiceHolder.prepareScreenshotService()
-    }
-
-    private fun shouldDisplayPlayButton(): Boolean {
-        val isLandscape = displayHelper.metrics.let { it.widthPixels >= it.heightPixels }
-
-        Timber.verbose { if (isLandscape) "LANDSCAPE" else "PORTRAIT" }
-
-        // Hide overlay in Portrait orientation (unless Surface Duo)
-        return isLandscape || prefsCore.gameAreaMode.get() == GameAreaMode.Duo
+        controller.onCreate()
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
-        if (shouldDisplayPlayButton()) {
-            overlay.show()
-        } else {
-            overlay.hide()
-
-            // Pause if script is running
-            scope.launch {
-                // This delay is to avoid race-condition with screen turn OFF listener
-                delay(Duration.seconds(1))
-
-                scriptManager.pause(ScriptManager.PauseAction.Pause).let { success ->
-                    if (success) {
-                        val msg = getString(R.string.script_paused)
-                        messages.toast(msg)
-                    }
-                }
-            }
-        }
+        controller.onScreenConfigChanged()
     }
 }
---
+++ app/src/main/java/com/mathewsachin/fategrandautomata/runner/ScriptRunnerServiceController.kt
@@ -0,0 +1,112 @@
+package com.mathewsachin.fategrandautomata.runner
+
+import android.app.Service
+import android.content.Context
+import com.mathewsachin.fategrandautomata.R
+import com.mathewsachin.fategrandautomata.prefs.core.GameAreaMode
+import com.mathewsachin.fategrandautomata.prefs.core.PrefsCore
+import com.mathewsachin.fategrandautomata.util.DisplayHelper
+import com.mathewsachin.fategrandautomata.util.ImageLoader
+import com.mathewsachin.fategrandautomata.util.ScreenOffReceiver
+import com.mathewsachin.fategrandautomata.util.ScriptMessages
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
+import kotlinx.coroutines.*
+import timber.log.Timber
+import timber.log.info
+import timber.log.verbose
+import javax.inject.Inject
+import kotlin.time.Duration
+
+@ServiceScoped
+class ScriptRunnerServiceController @Inject constructor(
+    private val service: Service,
+    @ApplicationContext private val context: Context,
+    private val scriptManager: ScriptManager,
+    private val screenshotServiceHolder: ScreenshotServiceHolder,
+    private val imageLoader: ImageLoader,
+    private val overlay: ScriptRunnerOverlay,
+    private val prefsCore: PrefsCore,
+    private val notification: ScriptRunnerNotification,
+    private val displayHelper: DisplayHelper,
+    private val messages: ScriptMessages,
+    private val messageBox: ScriptRunnerMessageBox
+) {
+    private val scope = CoroutineScope(Dispatchers.Default)
+    private val screenOffReceiver = ScreenOffReceiver()
+
+    fun onDestroy() {
+        Timber.info { "Script runner service destroyed" }
+
+        scriptManager.stopScript()
+        screenshotServiceHolder.close()
+
+        imageLoader.clearImageCache()
+
+        overlay.hide()
+        scope.cancel()
+
+        screenOffReceiver.unregister(service)
+    }
+
+    fun onCreate() {
+        Timber.info { "Script runner service created" }
+        notification.show()
+
+        screenOffReceiver.register(service) {
+            Timber.verbose { "SCREEN OFF" }
+
+            scriptManager.pause(ScriptManager.PauseAction.Pause).let { success ->
+                if (success) {
+                    val title = context.getString(R.string.script_paused)
+                    val msg = context.getString(R.string.screen_turned_off)
+                    messages.notify(msg)
+
+                    scope.launch {
+                        messageBox.show(title, msg)
+                    }
+                }
+            }
+        }
+
+        if (shouldDisplayPlayButton()) {
+            overlay.show()
+        }
+
+        screenshotServiceHolder.prepareScreenshotService()
+    }
+
+    fun onScreenConfigChanged() {
+        if (shouldDisplayPlayButton()) {
+            overlay.show()
+        } else {
+            overlay.hide()
+
+            // Pause if script is running
+            scope.launch {
+                // This delay is to avoid race-condition with screen turn OFF listener
+                delay(Duration.seconds(1))
+
+                scriptManager.pause(ScriptManager.PauseAction.Pause).let { success ->
+                    if (success) {
+                        val msg = context.getString(R.string.script_paused)
+                        messages.toast(msg)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun shouldDisplayPlayButton(): Boolean {
+        val isLandscape = displayHelper.metrics.let { it.widthPixels >= it.heightPixels }
+
+        Timber.verbose { if (isLandscape) "LANDSCAPE" else "PORTRAIT" }
+
+        // Hide overlay in Portrait orientation (unless Surface Duo)
+        return isLandscape || prefsCore.gameAreaMode.get() == GameAreaMode.Duo
+    }
+
+    fun onNewMediaProjectionToken() {
+        screenshotServiceHolder.prepareScreenshotService()
+    }
+}
