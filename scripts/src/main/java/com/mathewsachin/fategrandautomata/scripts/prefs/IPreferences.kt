6a035ff4037040a780d25fcd9c414daaefbb86b2
Kobata <kobata2@gmail.com>
2021-11-16 11:05:40 -0500

    New UI support for EN server (#951)

--- scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/prefs/IPreferences.kt
+++ scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/prefs/IPreferences.kt
@@ -50,4 +50,4 @@ interface IPreferences {
 
 val IPreferences.wantsMediaProjectionToken get() = !useRootForScreenshots
 
-val IPreferences.isNewUI get() = gameServer in listOf(GameServerEnum.Jp, GameServerEnum.Kr, GameServerEnum.Cn)
+val IPreferences.isNewUI get() = gameServer in listOf(GameServerEnum.Jp, GameServerEnum.En, GameServerEnum.Kr, GameServerEnum.Cn)
