abdfa8dac088a483da8c58ad120d55235daf3601
Kim, Jinsu <item4hun@gmail.com>
2022-08-28 12:32:29 +0900

    Move click position for click two type both

--- scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/locations/FPLocations.kt
+++ scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/locations/FPLocations.kt
@@ -11,7 +11,8 @@ class FPLocations @Inject constructor(
 ): IScriptAreaTransforms by scriptAreaTransforms {
     val summonCheck = Region(100, 1152, 75, 143).xFromCenter()
     val continueSummonRegion = Region(-36, 1264, 580, 170).xFromCenter()
-    val first10SummonClick = Location(120, 1062).xFromCenter()
+    // x: 160 is middle of first cost-free button and 2000 fp button on first screen
+    val first10SummonClick = Location(160, 1062).xFromCenter()
     val okClick = Location(320, 1120).xFromCenter()
     val continueSummonClick = Location(320, 1325).xFromCenter()
     val skipRapidClick = Location(1240, 1400).xFromCenter()
