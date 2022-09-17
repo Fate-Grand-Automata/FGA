42c4ed8bb822a352d7e5aed5a47fb91e024da27b
RandomArthur <16458204+arthurkun021@users.noreply.github.com>
2022-09-17 10:03:59 +0800

    CE bomb improvements by gary-stu

--- app/src/main/java/io/github/fate_grand_automata/ui/launcher/CEBombLauncher.kt
+++ app/src/main/java/io/github/fate_grand_automata/ui/launcher/CEBombLauncher.kt
@@ -2,35 +2,26 @@ package io.github.fate_grand_automata.ui.launcher
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import io.github.fate_grand_automata.scripts.prefs.IPreferences
-import io.github.fate_grand_automata.ui.dialog.ChoiceListItem
 
 @Composable
 fun ceBombLauncher(
     prefs: IPreferences,
     modifier: Modifier = Modifier
 ): ScriptLauncherResponseBuilder {
-    var target by remember { mutableStateOf(prefs.ceBombTargetRarity) }
-
     Column(
         modifier = modifier
             .padding(horizontal = 16.dp)
             .padding(top = 5.dp)
     ) {
         Text(
-            "Choose CE Bomb target",
+            "Start the CE Bomb script",
             style = MaterialTheme.typography.titleLarge
         )
 
@@ -40,24 +31,20 @@ fun ceBombLauncher(
                 .padding(bottom = 16.dp)
         )
 
-        LazyColumn {
-            items((1..2).toList()) {
-                ChoiceListItem(
-                    isSelected = target == it,
-                    onClick = { target = it }
-                ) {
-                    Text("$it\u2605 CEs")
-                }
-            }
-        }
+        Text(
+            "This script will consume any CE it can indiscriminately.\n" +
+                    "Please ensure you've locked every CE you don't want to lose, " +
+                    "or moved them to the Second Archive.",
+            style = MaterialTheme.typography.bodyLarge
+        )
     }
 
     return ScriptLauncherResponseBuilder(
         canBuild = { true },
         build = {
-            ScriptLauncherResponse.CEBomb(
-                targetRarity = target
-            )
+            // TODO : since the targetRarity doesn't matter anymore
+            // I'm just passing an arbitrary value as i have no idea how it works behind that
+            ScriptLauncherResponse.CEBomb(1)
         }
     )
 }
