6b49d8f1e7beb1bdf0d1378d6bcef139db07f408
reconman <reconman@users.noreply.github.com>
2023-06-30 22:33:18 +0200

    Remove unused CE images

--- app/src/main/java/io/github/fate_grand_automata/ui/launcher/CEBombLauncher.kt
+++ app/src/main/java/io/github/fate_grand_automata/ui/launcher/CEBombLauncher.kt
@@ -6,15 +6,11 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import io.github.fate_grand_automata.R
 import io.github.fate_grand_automata.scripts.prefs.IPreferences
-import io.github.fate_grand_automata.ui.ChoiceListItem
 
 @Composable
 fun ceBombLauncher(
