15811fca0f37be009ce979e48b26061af9bbc621
reconman <reconman@users.noreply.github.com>
2023-07-01 14:54:05 +0200

    Add play store update check

--- app/src/main/java/io/github/fate_grand_automata/ui/main/MainActivity.kt
+++ app/src/main/java/io/github/fate_grand_automata/ui/main/MainActivity.kt
@@ -5,9 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.core.view.WindowCompat
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.appupdate.AppUpdateOptions
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.fate_grand_automata.ui.pref_support.SupportViewModel
 import io.github.fate_grand_automata.util.CutoutManager
+import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -18,9 +24,10 @@ class MainActivity : ComponentActivity() {
     val vm: MainScreenViewModel by viewModels()
     val supportVm: SupportViewModel by viewModels()
 
+    private lateinit var appUpdateManager: AppUpdateManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
@@ -29,6 +36,9 @@ class MainActivity : ComponentActivity() {
                 supportVm = supportVm
             )
         }
+
+        appUpdateManager = AppUpdateManagerFactory.create(this)
+        checkForUpdate()
     }
 
     override fun onAttachedToWindow() {
@@ -39,5 +49,18 @@ class MainActivity : ComponentActivity() {
     override fun onStart() {
         super.onStart()
         vm.activityStarted()
+    }
+
+    private fun checkForUpdate() {
+        appUpdateManager.appUpdateInfo.addOnSuccessListener {
+            if (it.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE &&
+                it.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+            ) {
+                Timber.i("New version is available: ${it.availableVersionCode()}")
+                appUpdateManager.startUpdateFlowForResult(it, this, AppUpdateOptions.defaultOptions(AppUpdateType.IMMEDIATE), 100)
+            } else {
+                Timber.d("No update available")
+            }
+        }
     }
 }
