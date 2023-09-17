833ada31ca8bc2670cfa86f3454690451d525d52
Arthur <16458204+ArthurKun21@users.noreply.github.com>
2023-09-17 21:33:30 +0800

    fix the crashing with HorizontalPager and TabbedRow when recomposing with the page removed

--- app/src/main/java/io/github/fate_grand_automata/ui/Tabbed.kt
+++ app/src/main/java/io/github/fate_grand_automata/ui/Tabbed.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,8 +21,12 @@ fun <T> Tabbed(
     content: @Composable (T) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val pagerState = rememberPagerState()
+    val pagerState = rememberPagerState(initialPage = 0)
     val scope = rememberCoroutineScope()
+
+    LaunchedEffect(key1 = items, block ={
+        pagerState.scrollToPage(0)
+    })
 
     Column(
         modifier = modifier
