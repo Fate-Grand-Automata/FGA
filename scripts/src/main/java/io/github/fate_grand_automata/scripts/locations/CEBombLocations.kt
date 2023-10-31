195612cdcb363e93042469d752501acf7cfd7e96
arthur <16458204+ArthurKun21@users.noreply.github.com>
2023-10-31 19:06:51 +0800

    minor refactor

--- scripts/src/main/java/io/github/fate_grand_automata/scripts/locations/CEBombLocations.kt
+++ scripts/src/main/java/io/github/fate_grand_automata/scripts/locations/CEBombLocations.kt
@@ -9,47 +9,42 @@ class CEBombLocations @Inject constructor(
 ) : IScriptAreaTransforms by scriptAreaTransforms {
 
     // click on the center of previous region
-    val ceSelectCEToEnhanceLocation =
-        if (isWide)
-            Location(-900, 800).xFromCenter()
-        else
-            Location(400, 500)
+    val ceSelectCEToEnhanceLocation = when (isWide) {
+        true -> Location(-900, 800).xFromCenter()
+        false -> Location(400, 500)
+    }
+
 
     // The 20 CE grid located on the right of the selected CE to enhance
     // should be 20 empty dark gray "+" rectangles if no ce selected
-    val ceOpenEnhancementMenuLocation =
-        if (isWide)
-            Location(200, 500).xFromCenter()
-        else
-            Location(900, 500)
+    val ceOpenEnhancementMenuLocation = when (isWide) {
+        true -> Location(200, 500).xFromCenter()
+        false -> Location(900, 500)
+    }
 
     // on the CE selection screen, should be the top left CE
-    val ceFirstFodderLocation =
-        if (isWide)
-            Location(-980, 450).xFromCenter()
-        else
-            Location(280, 430)
+    val ceFirstFodderLocation = when (isWide) {
+        true -> Location(-980, 450).xFromCenter()
+        false -> Location(280, 430)
+    }
 
     // Ok button on the CE selection list
-    val ceUpgradeOkButton =
-        if (isWide)
-            Location(-400, 1300).xFromRight()
-        else
-            Location(2300, 1300)
+    val ceUpgradeOkButton = when (isWide) {
+        true -> Location(-400, 1300).xFromRight()
+        false -> Location(2300, 1300)
+    }
 
     // Ok button on the pop-up to ask if you want to use selected CEs to enhance
-    val cePerformEnhancementOkButton =
-        if (isWide)
-            Location(450, 1200).xFromCenter()
-        else
-            Location(1600, 1200)
+    val cePerformEnhancementOkButton = when (isWide) {
+        true -> Location(450, 1200).xFromCenter()
+        false -> Location(1600, 1200)
+    }
 
     // the "Multi Select" button on the CE Selection screen
-    val ceMultiSelectRegion =
-        if (isWide)
-            Region(175, 880, 135, 115)
-        else
-            Region(0, 880, 135, 115)
+    val ceMultiSelectRegion = when (isWide) {
+        true -> Region(175, 880, 135, 115)
+        false -> Region(0, 880, 135, 115)
+    }
 
     /**
      * This is checking the space in between CEs
