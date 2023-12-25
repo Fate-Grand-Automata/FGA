f0f03857fb2fd5ba8da42cf6604491fdc7908465
arthur <16458204+ArthurKun21@users.noreply.github.com>
2023-12-25 16:33:24 +0800

    forgot to remove this code

--- app/src/main/java/io/github/fate_grand_automata/ui/battle_config_item/Materials.kt
+++ app/src/main/java/io/github/fate_grand_automata/ui/battle_config_item/Materials.kt
@@ -3,10 +3,8 @@ package io.github.fate_grand_automata.ui.battle_config_item
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
@@ -17,12 +15,8 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
@@ -32,10 +26,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import io.github.fate_grand_automata.R
 import io.github.fate_grand_automata.prefs.core.BattleConfigCore
-import io.github.fate_grand_automata.prefs.core.Pref
 import io.github.fate_grand_automata.scripts.enums.MaterialEnum
-import io.github.fate_grand_automata.ui.dialog.FgaDialog
-import io.github.fate_grand_automata.ui.dialog.multiChoiceList
 import io.github.fate_grand_automata.ui.prefs.remember
 import io.github.fate_grand_automata.util.drawable
 import io.github.fate_grand_automata.util.stringRes
@@ -67,72 +58,6 @@ fun MaterialDisplay(
     }
 }
 
-@Composable
-fun Pref<Set<MaterialEnum>>.Materials() {
-    var selected by remember()
-
-    val dialog = FgaDialog()
-
-    dialog.build {
-        var current by remember(selected) { mutableStateOf(selected) }
-
-        Row {
-            Box(
-                modifier = Modifier
-                    .weight(1f)
-                    .alignByBaseline()
-            ) {
-                title(stringResource(R.string.p_mats))
-            }
-
-            TextButton(
-                onClick = { current = emptySet() },
-                modifier = Modifier
-                    .padding(16.dp, 5.dp)
-                    .alignByBaseline(),
-                enabled = current.isNotEmpty()
-            ) {
-                // TODO: Localize
-                Text("CLEAR")
-            }
-        }
-
-        multiChoiceList(
-            selected = current,
-            onSelectedChange = { current = it },
-            prioritySelected = true,
-            items = MaterialEnum.values().toList()
-        ) { mat ->
-            Material(mat)
-
-            Text(
-                stringResource(mat.stringRes),
-                modifier = Modifier
-                    .padding(start = 16.dp)
-            )
-        }
-
-        buttons(
-            onSubmit = { selected = current }
-        )
-    }
-
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable { dialog.show() }
-            .heightIn(min = 70.dp)
-            .padding(vertical = 5.dp)
-    ) {
-        Text(
-            stringResource(R.string.p_mats).uppercase(),
-            style = MaterialTheme.typography.bodySmall,
-            modifier = Modifier.padding(start = 16.dp)
-        )
-
-        MaterialsSummary(materials = selected.toList())
-    }
-}
 
 @Composable
 fun Material(mat: MaterialEnum) {
