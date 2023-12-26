10bc626c8f6c38c3ce98271e25ebba81eb20a91c
arthur <16458204+ArthurKun21@users.noreply.github.com>
2023-12-26 11:57:07 +0800

    added on click search material on the top bar

--- app/src/main/java/io/github/fate_grand_automata/ui/material/MaterialScreen.kt
+++ app/src/main/java/io/github/fate_grand_automata/ui/material/MaterialScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -308,7 +309,14 @@ private fun MaterialQuery(
             } else {
                 Row(
                     modifier = Modifier
-                        .fillMaxWidth(),
+                        .fillMaxWidth()
+                        .clickable(
+                            onClick = {
+                                isSearchModeOn = true
+                            },
+                            indication = null,
+                            interactionSource = remember { MutableInteractionSource() }
+                        ),
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.End
                 ) {
