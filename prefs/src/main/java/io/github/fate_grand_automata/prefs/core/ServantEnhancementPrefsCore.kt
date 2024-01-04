8ded3b5c124be3786bf4bb0e9a3ae80fc66bae5e
arthur <16458204+ArthurKun021@users.noreply.github.com>
2024-01-05 00:07:42 +0800

    updated the preferences to have prefix of `level_` for easier tracking

--- prefs/src/main/java/io/github/fate_grand_automata/prefs/core/ServantEnhancementPrefsCore.kt
+++ prefs/src/main/java/io/github/fate_grand_automata/prefs/core/ServantEnhancementPrefsCore.kt
@@ -2,15 +2,15 @@ package io.github.fate_grand_automata.prefs.core
 
 class ServantEnhancementPrefsCore(maker: PrefMaker) {
 
-    val shouldLimit = maker.bool("limit_servant_enhancement")
+    val shouldLimit = maker.bool("level_limit_servant_enhancement")
 
-    val limitCount = maker.stringAsInt("limit_servant_enhancement_count", default = 1)
+    val limitCount = maker.stringAsInt("level_limit_servant_enhancement_count", default = 1)
 
-    val shouldRedirectAscension = maker.bool("redirect_servant_ascension")
+    val shouldRedirectAscension = maker.bool("level_redirect_servant_ascension")
 
-    val shouldPerformAscension = maker.bool("perform_servant_ascension")
+    val shouldPerformAscension = maker.bool("level_perform_servant_ascension")
 
-    val shouldRedirectGrail = maker.bool("redirect_servant_grail")
+    val shouldRedirectGrail = maker.bool("level_redirect_servant_grail")
 
-    val muteNotifications = maker.bool("mute_servant_enhancement_notifications")
+    val muteNotifications = maker.bool("level_mute_servant_enhancement_notifications")
 }
