fbd4b8c5c19a0d942ffe56f8efe491ca36e68daa
arthur <16458204+ArthurKun021@users.noreply.github.com>
2024-01-07 02:41:57 +0800

    added documentation

--- prefs/src/main/java/io/github/fate_grand_automata/prefs/core/SkillPrefsCore.kt
+++ prefs/src/main/java/io/github/fate_grand_automata/prefs/core/SkillPrefsCore.kt
@@ -1,5 +1,29 @@
 package io.github.fate_grand_automata.prefs.core
 
+/**
+ * @param shouldUpgradeSkillOne: Whether to upgrade skill one
+ * @param shouldUpgradeSkillTwo: Whether to upgrade skill two
+ * @param shouldUpgradeSkillThree: Whether to upgrade skill three
+ *
+ * @param minimumSkillOne: Minimum skill level for skill one.
+ * @param minimumSkillTwo: Minimum skill level for skill two
+ * @param minimumSkillThree: Minimum skill level for skill three
+ *        Serves as the baseline for skill leveling.
+ *
+ * @param skillOneUpgradeValue: Skill level to upgrade skill one to
+ * @param skillTwoUpgradeValue: Skill level to upgrade skill two to
+ * @param skillThreeUpgradeValue: Skill level to upgrade skill three to
+ *          This value is added to the minimum skill level.
+ *          For example, if minimumSkillOne is 1 and skillOneUpgradeValue is 2,
+ *          then skill one will be upgraded to level 3.
+ *          The Script would perform two loops of skill one upgrade.
+ *
+ * @param isSkillTwoAvailable: Whether skill two is available
+ * @param isSkillThreeAvailable: Whether skill three is available
+ *           Skill one is always available. This takes into account that the
+ *          servant may not have skill two or three. Due to being in lower
+ *          ascension or not being leveled up.
+ */
 class SkillPrefsCore(maker: PrefMaker) {
 
     val shouldUpgradeSkillOne = maker.bool("skill_should_upgrade_one")
@@ -10,7 +34,6 @@ class SkillPrefsCore(maker: PrefMaker) {
     val minimumSkillTwo = maker.stringAsInt(key="skill_two_minimum", default = 1)
     val skillTwoUpgradeValue = maker.stringAsInt(key = "skill_two_upgrade_value")
     val isSkillTwoAvailable = maker.bool("skill_is_two_available")
-
 
     val shouldUpgradeSkillThree = maker.bool("skill_should_upgrade_three")
     val minimumSkillThree = maker.stringAsInt(key="skill_three_minimum", default = 1)
