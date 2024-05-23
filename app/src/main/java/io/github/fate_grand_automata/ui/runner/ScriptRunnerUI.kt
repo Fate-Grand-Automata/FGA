cc742f2287dcd5fe7185eba41dcaf2fa6bd1cf3d
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2024-05-23 10:11:01 +0800

    forgot the import

--- app/src/main/java/io/github/fate_grand_automata/ui/runner/ScriptRunnerUI.kt
+++ app/src/main/java/io/github/fate_grand_automata/ui/runner/ScriptRunnerUI.kt
@@ -14,12 +14,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import io.github.fate_grand_automata.R
