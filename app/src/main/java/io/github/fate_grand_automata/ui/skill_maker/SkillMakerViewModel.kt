06ef32cf76ca62619029881235b853621ea535e2
arthur <16458204+ArthurKun021@users.noreply.github.com>
2024-06-07 20:59:13 +0800

    On tap, return to the last index

--- app/src/main/java/io/github/fate_grand_automata/ui/skill_maker/SkillMakerViewModel.kt
+++ app/src/main/java/io/github/fate_grand_automata/ui/skill_maker/SkillMakerViewModel.kt
@@ -56,7 +56,7 @@ class SkillMakerViewModel @Inject constructor(
         }
     )
     private val _turn = mutableIntStateOf(
-        if (state.skillString != null){
+        if (state.skillString != null) {
             state.turn
         } else {
             model.skillCommand.count { it is SkillMakerEntry.Next } + 1
@@ -285,7 +285,7 @@ class SkillMakerViewModel @Inject constructor(
             prevStage()
             prevTurn()
         }
-        if (last is SkillMakerEntry.Next.Turn){
+        if (last is SkillMakerEntry.Next.Turn) {
             prevTurn()
         }
 
@@ -301,6 +301,10 @@ class SkillMakerViewModel @Inject constructor(
         while (!isEmpty()) {
             onDeleteSelected()
         }
+    }
+
+    fun clearSelection() {
+        _currentIndex.value = model.skillCommand.lastIndex
     }
 
     fun onDeleteSelected() {
