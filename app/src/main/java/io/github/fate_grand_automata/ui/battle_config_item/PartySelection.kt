d458390a7ea5d9b7767c803b17c65723be17a02e
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2024-08-06 05:22:03 +0800

    missing imports

--- app/src/main/java/io/github/fate_grand_automata/ui/battle_config_item/PartySelection.kt
+++ app/src/main/java/io/github/fate_grand_automata/ui/battle_config_item/PartySelection.kt
@@ -19,6 +19,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
