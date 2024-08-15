9669457f85454aa4af341e63b291a0a6c65d1d8a
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2024-08-15 16:19:53 +0800

    fix

--- app/src/main/java/io/github/fate_grand_automata/ui/skill_maker/SkillMakerMain.kt
+++ app/src/main/java/io/github/fate_grand_automata/ui/skill_maker/SkillMakerMain.kt
@@ -129,7 +129,7 @@ fun SkillMakerMain(
                 )
             }
 
-            val commandSpellRemaining by vm.commandSpell.collectAsState()
+            val commandSpellRemaining by vm.commandSpell
 
             Column(
                 verticalArrangement = Arrangement.Bottom,
