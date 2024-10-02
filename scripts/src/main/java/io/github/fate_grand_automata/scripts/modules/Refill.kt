2e07945d9b73001b1013416abb300be8a0915ab7
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2024-10-02 11:14:51 +0800

    wait for AP regen on the last run

--- scripts/src/main/java/io/github/fate_grand_automata/scripts/modules/Refill.kt
+++ scripts/src/main/java/io/github/fate_grand_automata/scripts/modules/Refill.kt
@@ -4,6 +4,7 @@ import io.github.fate_grand_automata.scripts.IFgoAutomataApi
 import io.github.fate_grand_automata.scripts.Images
 import io.github.fate_grand_automata.scripts.ScriptNotify
 import io.github.fate_grand_automata.scripts.entrypoints.AutoBattle
+import io.github.fate_grand_automata.scripts.prefs.IPerServerConfigPrefs
 import io.github.lib_automata.dagger.ScriptScope
 import javax.inject.Inject
 import kotlin.time.Duration.Companion.seconds
@@ -19,34 +20,61 @@ class Refill @Inject constructor(
      * Refills the AP with apples depending on preferences.
      * If needed, loops and wait for AP regeneration
      */
-    private fun refillOnce() {
+    private fun refillOnce(
+        isLastRun: Boolean = false
+    ) {
         val perServerConfigPref = prefs.selectedServerConfigPref
 
-        if (perServerConfigPref.resources.isNotEmpty()
-            && timesRefilled < perServerConfigPref.currentAppleCount
-        ) {
-            //TODO check for OK image between each resource
-            perServerConfigPref.resources
-                .flatMap { locations.locate(it) }
-                .forEach { it.click() }
+        when {
+            /**
+             * If the user has resources to refill and has the wait for AP regen option enabled
+             * and this is the last run, wait for AP regen instead of refilling.
+             */
+            perServerConfigPref.waitForAPRegen && isLastRun -> waitForAPRegen()
+            /**
+             * If the user has resources to refill and has not reached the current apple count,
+             */
+            perServerConfigPref.resources.isNotEmpty() && timesRefilled < perServerConfigPref.currentAppleCount -> {
+                refillAP(perServerConfigPref = perServerConfigPref)
+            }
+            /**
+             * wait for AP regen if the user has the wait for AP regen option enabled
+             */
 
-            1.seconds.wait()
-            locations.staminaOkClick.click()
-            ++timesRefilled
+            perServerConfigPref.waitForAPRegen -> waitForAPRegen()
 
-            3.seconds.wait()
-        } else if (perServerConfigPref.waitForAPRegen) {
-            locations.staminaCloseClick.click()
-
-            messages.notify(ScriptNotify.WaitForAPRegen())
-
-            60.seconds.wait()
-        } else throw AutoBattle.BattleExitException(AutoBattle.ExitReason.APRanOut)
+            else -> throw AutoBattle.BattleExitException(AutoBattle.ExitReason.APRanOut)
+        }
     }
 
-    fun refill() {
+    private fun refillAP(perServerConfigPref: IPerServerConfigPrefs) {
+        //TODO check for OK image between each resource
+        perServerConfigPref.resources
+            .flatMap { locations.locate(it) }
+            .forEach { it.click() }
+
+        1.seconds.wait()
+        locations.staminaOkClick.click()
+        ++timesRefilled
+
+        3.seconds.wait()
+    }
+
+    private fun waitForAPRegen() {
+        locations.staminaCloseClick.click()
+
+        messages.notify(ScriptNotify.WaitForAPRegen())
+
+        60.seconds.wait()
+    }
+
+    fun refill(
+        isLastRun: Boolean = false
+    ) {
         if (images[Images.Stamina] in locations.staminaScreenRegion) {
-            refillOnce()
+            refillOnce(
+                isLastRun = isLastRun
+            )
         }
     }
 
