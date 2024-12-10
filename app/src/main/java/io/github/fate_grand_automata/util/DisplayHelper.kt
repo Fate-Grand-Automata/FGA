10ecc5562989a05b0e896fd5016ce9554f45b98b
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2024-12-10 22:07:15 +0800

    Fix deprecated display metrics

--- app/src/main/java/io/github/fate_grand_automata/util/DisplayHelper.kt
+++ app/src/main/java/io/github/fate_grand_automata/util/DisplayHelper.kt
@@ -1,19 +1,39 @@
 package io.github.fate_grand_automata.util
 
+import android.content.Context
+import android.content.res.Resources
+import android.hardware.display.DisplayManager
 import android.util.DisplayMetrics
+import android.view.Display
 import android.view.WindowManager
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class DisplayHelper @Inject constructor(
+    @ApplicationContext val context: Context,
     windowManager: WindowManager
 ) {
+    @Suppress("DEPRECATION")
     private val display = windowManager.defaultDisplay
 
     val metrics: DisplayMetrics
-        get() =
-        DisplayMetrics().also { display.getRealMetrics(it) }
+        get() = if (display != null) {
+            DisplayMetrics().also {
+                @Suppress("DEPRECATION")
+                display.getRealMetrics(it)
+            }
+        } else {
+            Resources.getSystem().displayMetrics
+        }
 
-    val rotation get() = display.rotation
+    val rotation: Int
+        get() = if (display != null) {
+            display.rotation
+        } else {
+            val displayManager = context.getSystemService(DisplayManager::class.java)
+            val displayM = displayManager.getDisplay(Display.DEFAULT_DISPLAY)
+            displayM.rotation
+        }
 }
