e83593f2d85a16781d86f0930b8121d3184fc252
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2025-01-03 16:33:46 +0800

    revert changes

--- scripts/src/main/java/io/github/fate_grand_automata/scripts/entrypoints/AutoDetect.kt
+++ scripts/src/main/java/io/github/fate_grand_automata/scripts/entrypoints/AutoDetect.kt
@@ -17,8 +17,8 @@ class AutoDetect @Inject constructor(
 
         when {
             images[Images.FriendSummon] in locations.fp.summonCheck ||
-                findImage(locations.fp.continueSummonRegion, Images.FPSummonContinue) ||
-                images[Images.FriendSummon] in locations.fp.initialSummonCheck ->
+                    findImage(locations.fp.continueSummonRegion, Images.FPSummonContinue) ||
+                    images[Images.FriendSummon] in locations.fp.initialSummonCheck ->
                 ScriptModeEnum.FP
 
             images[Images.LotteryBoxFinished] in locations.lottery.checkRegion || images[Images.LotteryBoxFinished] in locations.lottery.finishedRegion ->
