24612b5a34a554b46653fb81335aa4be520ff9eb
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2025-05-09 06:37:32 +0800

    chore: update the display helper

--- app/src/main/java/io/github/fate_grand_automata/util/DisplayHelper.kt
+++ app/src/main/java/io/github/fate_grand_automata/util/DisplayHelper.kt
@@ -1,8 +1,8 @@
 package io.github.fate_grand_automata.util
 
 import android.content.Context
-import android.content.res.Resources
 import android.hardware.display.DisplayManager
+import android.os.Build
 import android.util.DisplayMetrics
 import android.view.Display
 import android.view.WindowManager
@@ -13,27 +13,38 @@ import javax.inject.Singleton
 @Singleton
 class DisplayHelper @Inject constructor(
     @ApplicationContext val context: Context,
-    windowManager: WindowManager
+    private val windowManager: WindowManager,
 ) {
     @Suppress("DEPRECATION")
     private val display = windowManager.defaultDisplay
 
     val metrics: DisplayMetrics
-        get() = if (display != null) {
+        get() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            val bounds = windowManager.currentWindowMetrics.bounds
+            val contextMetrics = context.resources.displayMetrics
+
+            val defaultMetrics = DisplayMetrics().apply {
+                widthPixels = bounds.width()
+                heightPixels = bounds.height()
+                density = contextMetrics.density // Fix: Use context's display metrics
+                densityDpi = contextMetrics.densityDpi
+            }
+
+            defaultMetrics
+        } else {
             DisplayMetrics().also {
                 @Suppress("DEPRECATION")
                 display.getRealMetrics(it)
             }
-        } else {
-            Resources.getSystem().displayMetrics
         }
 
     val rotation: Int
-        get() = if (display != null) {
-            display.rotation
-        } else {
+        get() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             val displayManager = context.getSystemService(DisplayManager::class.java)
             val displayM = displayManager.getDisplay(Display.DEFAULT_DISPLAY)
-            displayM.rotation
+            displayM?.rotation ?: 0
+        } else {
+            @Suppress("DEPRECATION")
+            display?.rotation ?: 0
         }
 }
