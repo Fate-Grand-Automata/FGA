3f9758ae3c17f28bf7cec93e72156edbeed5f428
reconman <reconman@users.noreply.github.com>
2025-05-10 23:41:03 +0200

    Remove develocity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,3 @@
-plugins {
-    id("com.gradle.develocity") version("4.0.1")
-}
-develocity {
-    buildScan {
-        termsOfUseUrl = "https://gradle.com/terms-of-service"
-        termsOfUseAgree = "yes"
-    }
-}
 include(":prefs")
 include(":scripts")
 include(":libautomata")
