cbbe1248bf686a074755b76fb72632a2f3940e92
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2025-05-13 21:12:48 +0000

    refactor: add plugin management and dependency resolution management to settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,48 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("androidx\\..*")
+                includeGroupByRegex("com\\.android(\\..*|)")
+                includeGroupByRegex("com\\.google\\.android\\..*")
+                includeGroupByRegex("com\\.google\\.testing\\.platform")
+            }
+            mavenContent {
+                releasesOnly()
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("androidx\\..*")
+                includeGroupByRegex("com\\.android(\\..*|)")
+                includeGroupByRegex("com\\.google\\.android\\..*")
+                includeGroupByRegex("com\\.google\\.testing\\.platform")
+            }
+            mavenContent {
+                releasesOnly()
+            }
+        }
+        // fallback for the rest of the dependencies
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
 include(":prefs")
 include(":scripts")
 include(":libautomata")
 include(":app")
 
 rootProject.name="Fate Grand Automata"
-
