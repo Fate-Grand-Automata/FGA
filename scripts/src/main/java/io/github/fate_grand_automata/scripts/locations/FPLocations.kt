b507e60682176fd8d0e857a8ea282abe76d9a2e7
reconman <reconman@users.noreply.github.com>
2025-07-29 20:44:55 +0200

    Simplify Anni 9 condition, also fix FP summons

--- scripts/src/main/java/io/github/fate_grand_automata/scripts/locations/FPLocations.kt
+++ scripts/src/main/java/io/github/fate_grand_automata/scripts/locations/FPLocations.kt
@@ -9,24 +9,28 @@ import javax.inject.Inject
 @ScriptScope
 class FPLocations @Inject constructor(
     scriptAreaTransforms: IScriptAreaTransforms
-): IScriptAreaTransforms by scriptAreaTransforms {
-    val summonCheck = when (gameServer) {
-        is GameServer.Jp -> Region(250, 1121, 100, 143).xFromCenter()
-        else -> Region(100, 1152, 75, 143).xFromCenter()
-    }
+) : IScriptAreaTransforms by scriptAreaTransforms {
+    // 9th anniversary adds 100x summon to the FP screen
+    private val afterAnni9 = gameServer is GameServer.Jp || gameServer is GameServer.Cn
+
+    val summonCheck = if (afterAnni9)
+        Region(250, 1121, 100, 143).xFromCenter()
+    else
+        Region(100, 1152, 75, 143).xFromCenter()
 
     val initialSummonCheck = Region(-305, 1121, 240, 183).xFromCenter()
-    val initialSummonClick = when (gameServer) {
-        is GameServer.Jp -> Location(2, 977).xFromCenter()
-        else -> Location(2, 1052).xFromCenter()
-    }
+    val initialSummonClick = if (afterAnni9)
+        Location(2, 977).xFromCenter()
+    else
+        Location(2, 1052).xFromCenter()
 
     val continueSummonRegion = Region(-36, 1264, 580, 170).xFromCenter()
-    val first10SummonClick = when (gameServer) {
+    val first10SummonClick = if (afterAnni9)
         // the old location is still valid but only for 10x FP
-        is GameServer.Jp -> Location(400, 1062).xFromCenter()
-        else -> Location(120, 1062).xFromCenter()
-    }
+        Location(400, 1062).xFromCenter()
+    else
+        Location(120, 1062).xFromCenter()
+
     val okClick = Location(320, 1120).xFromCenter()
     val continueSummonClick = Location(320, 1325).xFromCenter()
     val skipRapidClick = Location(1240, 1400).xFromCenter()
