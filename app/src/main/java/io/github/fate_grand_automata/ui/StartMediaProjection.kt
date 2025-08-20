371c29aa5cdd76bc91bc77af751e6f18a273812f
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2025-08-21 06:22:53 +0800

    feat: add support for full screen capture for android 15 and above

--- app/src/main/java/io/github/fate_grand_automata/ui/StartMediaProjection.kt
+++ app/src/main/java/io/github/fate_grand_automata/ui/StartMediaProjection.kt
@@ -3,14 +3,20 @@ package io.github.fate_grand_automata.ui
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.media.projection.MediaProjectionConfig
 import android.media.projection.MediaProjectionManager
+import android.os.Build
 import androidx.activity.result.contract.ActivityResultContract
 
 class StartMediaProjection : ActivityResultContract<Unit, Intent?>() {
     override fun createIntent(context: Context, input: Unit): Intent {
         val mediaProjectionManager = context.getSystemService(MediaProjectionManager::class.java)
 
-        return mediaProjectionManager.createScreenCaptureIntent()
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            mediaProjectionManager.createScreenCaptureIntent(MediaProjectionConfig.createConfigForDefaultDisplay())
+        } else {
+            mediaProjectionManager.createScreenCaptureIntent()
+        }
     }
 
     override fun parseResult(resultCode: Int, intent: Intent?) =
