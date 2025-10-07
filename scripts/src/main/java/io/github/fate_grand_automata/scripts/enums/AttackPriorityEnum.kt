160b720421f86dd98453377497de793bcfbe49b8
Vylantze <digi9346@hotmail.com>
2025-10-07 18:09:33 +0800

    Update with a comment on the redundancy of the defaultOrder.

--- scripts/src/main/java/io/github/fate_grand_automata/scripts/enums/AttackPriorityEnum.kt
+++ scripts/src/main/java/io/github/fate_grand_automata/scripts/enums/AttackPriorityEnum.kt
@@ -7,6 +7,9 @@ enum class AttackPriorityEnum {
     CardColorPriority;
 
     companion object {
+        // It is defined separately here instead of using the order of the enum definition
+        // because some parts might be removed or changed in future when the actual AttackPriority implementation is done
+        // and this will prevent the app from breaking when serializing or deserializing content from this enum.
         val defaultOrder = listOf(BraveChainPriority, CardChainPriority, ServantPriority, CardColorPriority)
     }
 }
