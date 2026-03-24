836b69bc01915b2d355ee36b9e577232c2027a81
vybze <32293918+vybze@users.noreply.github.com>
2026-03-23 21:44:01 -0500

    Update BattleScreenLocations.kt

--- scripts/src/main/java/io/github/fate_grand_automata/scripts/locations/BattleScreenLocations.kt
+++ scripts/src/main/java/io/github/fate_grand_automata/scripts/locations/BattleScreenLocations.kt
@@ -120,7 +120,7 @@ class BattleScreenLocations @Inject constructor(
     
     val servantDetailsFaceCardRegion = when (gameServer) {
         // FGO JP 2024-04-12 updated the UI resulting in the bricking of the face card detection
-        is GameServer.Jp, GameServer.Cn, is GameServer.Kr, is GameServer.Na -> Region(-685, 410, 110, 60).xFromCenter()
+        is GameServer.Jp, GameServer.Cn, is GameServer.Kr, is GameServer.En -> Region(-685, 410, 110, 60).xFromCenter()
         else -> Region(-685, 330, 110, 60).xFromCenter()
     }
 
