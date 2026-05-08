1e49a20f849c6bc6d3cc640adb23cdbd2eb0b4bc
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2026-05-08 17:29:30 +0000

    nav3 deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -138,6 +138,9 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.hilt.navigation.compose)
 
+    implementation(libs.androidx.navigation3.runtime)
+    implementation(libs.androidx.navigation3.ui)
+    implementation(libs.androidx.lifecycle.viewmodel.navigation3)
 
 
     implementation(libs.accompanist.permissions)
