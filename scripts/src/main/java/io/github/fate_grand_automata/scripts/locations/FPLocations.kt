4d082817811aedd3a4a46156d2c66e852b8470cc
reconman <reconman@users.noreply.github.com>
2026-09-14 16:32:08 +0200

    Fix FP summon for TW

--- scripts/src/main/java/io/github/fate_grand_automata/scripts/locations/FPLocations.kt
+++ scripts/src/main/java/io/github/fate_grand_automata/scripts/locations/FPLocations.kt
@@ -1,6 +1,5 @@
 package io.github.fate_grand_automata.scripts.locations
 
-import io.github.fate_grand_automata.scripts.enums.GameServer
 import io.github.lib_automata.Location
 import io.github.lib_automata.Region
 import io.github.lib_automata.dagger.ScriptScope
@@ -10,27 +9,14 @@ import javax.inject.Inject
 class FPLocations @Inject constructor(
     scriptAreaTransforms: IScriptAreaTransforms
 ) : IScriptAreaTransforms by scriptAreaTransforms {
-    // 9th anniversary adds 100x summon to the FP screen
-    private val afterAnni9 = gameServer is GameServer.Jp || gameServer is GameServer.Cn || gameServer is GameServer.Kr || gameServer is GameServer.En
-
-    val summonCheck = if (afterAnni9)
-        Region(250, 1121, 100, 143).xFromCenter()
-    else
-        Region(100, 1152, 75, 143).xFromCenter()
+    val summonCheck = Region(250, 1121, 100, 143).xFromCenter()
 
     val initialSummonCheck = Region(-305, 1121, 240, 183).xFromCenter()
-    val initialSummonClick = if (afterAnni9)
-        Location(2, 977).xFromCenter()
-    else
-        Location(2, 1052).xFromCenter()
+    val initialSummonClick = Location(2, 977).xFromCenter()
     val initialSummonContinueClick = Region(341, 1145, 120, 60).xFromCenter()
 
     val continueSummonRegion = Region(-36, 1264, 580, 170).xFromCenter()
-    val first10SummonClick = if (afterAnni9)
-        // the old location is still valid but only for 10x FP
-        Location(400, 1062).xFromCenter()
-    else
-        Location(170, 1062).xFromCenter()
+    val first10SummonClick = Location(400, 1062).xFromCenter()
 
     val okClick = Location(320, 1120).xFromCenter()
     val continueSummonClick = Location(320, 1325).xFromCenter()
